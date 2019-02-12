9283055c40727a81a68ba51304e903f370210076
amuhammad <amuhammad@ctacorp.com>
2019-02-12 13:32:16 -0500

    vbox guest addition fix mod script & packer template change

--- packer-build/mods/init.sh
+++ packer-build/mods/init.sh
@@ -7,4 +7,10 @@ sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 # Disable daily apt unattended updates.
 echo 'APT::Periodic::Enable "0";' >> /etc/apt/apt.conf.d/10periodic
 
-apt update -y
+apt-get update -y
+
+# Install necessary libraries for guest additions and Vagrant NFS Share
+sudo apt-get install linux-headers-$(uname -r) build-essential dkms nfs-common -y
+
+# Install necessary dependencies
+sudo apt-get install curl wget git tmux xvfb vim -y
---
+++ packer-build/mods/vagrant_init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -eux
+
+# Mod for setting up vagrant keys.
+
+mkdir ~/.ssh
+chmod 700 ~/.ssh
+cd ~/.ssh
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+chown -R vagrant ~/.ssh
+
+
+# Custom VBoxAdditions mount and setup required by vagrant
+mkdir -p /mnt/virtualbox
+mount -o loop /home/vagrant/VBoxGuest*.iso /mnt/virtualbox
+sh /mnt/virtualbox/VBoxLinuxAdditions.run
+ln -s /opt/VBoxGuestAdditions-*/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
+umount /mnt/virtualbox
+rm -rf /home/vagrant/VBoxGuest*.iso
