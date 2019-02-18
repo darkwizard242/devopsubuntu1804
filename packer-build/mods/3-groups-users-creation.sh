8303924be75d171cff80e9fc5523b818f8676053
Tech-Overlord <am900820@gmail.com>
2019-02-17 22:15:09 -0500

    set default shell for users; modify hostname/domain (fqdn)

--- packer-build/mods/3-groups-users-creation.sh
+++ packer-build/mods/3-groups-users-creation.sh
@@ -26,7 +26,7 @@ do
       usermod -aG docker $username
     else
       echo "The user: $username does not exist. Creating user: $username !"
-      useradd $username -m -p $(openssl passwd -1 password) -g $username
+      useradd $username -m -s /bin/bash -p $(openssl passwd -1 password) -g $username
       echo -e "Adding user: $username to sudo group." && usermod -aG sudo $username
       echo -e "Adding user: $username to docker group." && usermod -aG docker $username
       echo -e
