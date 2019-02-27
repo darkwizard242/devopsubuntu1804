caf9cc0d880bbb793af9c93351b35d4cd9f220a2
Tech-Overlord <am900820@gmail.com>
2019-02-27 14:00:42 -0500

    logic installation of ruby and gems such as serverspec

---
+++ packer-build/mods/13-install_ruby.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -eux
+
+## Install Ruby
+packages="ruby"
+
+for package in $packages;
+do
+  dpkg -s $package &> /dev/null && echo -e
+  if [ $? -eq 0 ];
+    then
+      echo "$package is already available and installed within the system" && echo -e
+    else
+      echo "About to install $package" && echo -e
+      DEBIAN_FRONTEND=non-interactive apt-get install $package -y
+  fi
+done
+
+## Install ServerSpec
+gems_to_install="serverspec"
+ruby --version
+if [ $? -eq 0 ];
+then
+  echo "Installing $gems_to_install" 
+  gem install $gems_to_install
+else
+  echo "Ruby is not installed."
+fi
