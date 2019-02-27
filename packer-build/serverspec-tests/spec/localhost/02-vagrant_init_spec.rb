561ae46071675261a7cf6a98ae839903345c8647
Tech-Overlord <am900820@gmail.com>
2019-02-27 13:30:00 -0500

    spec-check for vagrant .ssh folder

---
+++ packer-build/serverspec-tests/spec/localhost/02-vagrant_init_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+## Validate .ssh folder for vagrant.
+describe file('/home/vagrant/.ssh') do
+  it { should exist }
+  it { should be_directory }
+  it { should be_owned_by 'vagrant' }
+  it { should be_mode 700 }
+end
+
