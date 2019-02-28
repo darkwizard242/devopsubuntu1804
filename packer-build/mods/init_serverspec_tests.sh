fd8e7463744899a6051777d1c149faad7cf1b08d
Tech-Overlord <am900820@gmail.com>
2019-02-28 13:50:07 -0500

    initialization of serverspec tests for devopsubuntu1804

---
+++ packer-build/mods/init_serverspec_tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -eux
+
+serverspec_dest_dir="/opt/serverspec-tests"
+
+## Rsync serverspec-tests directory to /opt and change ownership & permissions
+## Execute ServerSpec tests for devopsubuntu1804
+rsync -avzh /home/vagrant/serverspec-tests /opt/
+chown -R root:root /opt/serverspec-tests
+cd $serverspec_dest_dir
+
+if [ "$PWD" = "$serverspec_dest_dir" ];
+  then
+    echo -e "\n\tCurrent Directory is: $PWD \n"
+    echo -e "\n\tExecuting ServerSpec tests: \n"
+    rake spec:localhost
+  else
+    echo -e "\nCurrent Directory is $PWD and not the correct directory to run server-spec tests in!\n"
+    echo -e "\nChanging to desired directory: serverspec_dest_dir \n" && cd $serverspec_dest_dir
+    echo -e "\n\tCurrent Directory is: $PWD \n"
+    echo -e "\n\tExecuting ServerSpec tests: \n"
+    rake spec:localhost
+fi
+
