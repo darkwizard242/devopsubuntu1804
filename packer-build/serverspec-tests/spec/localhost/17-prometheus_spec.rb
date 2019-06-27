ef9850092ad01447a30eb62d11f4a2bb94b2c9e6
darkwizard242 <am900820@gmail.com>
2019-06-27 17:02:42 -0400

    refactored serverspec infra test for prometheus

--- packer-build/serverspec-tests/spec/localhost/17-prometheus_spec.rb
+++ packer-build/serverspec-tests/spec/localhost/17-prometheus_spec.rb
@@ -9,16 +9,51 @@ describe port(9090), :if => os[:family] == 'ubuntu' do
   it { should be_listening }
 end
 
-describe file('/usr/local/bin/prometheus'), :if => os[:family] == 'ubuntu' do
-  it { should be_file }
+describe user('prometheus') do
+  it { should exist }
+  it { should have_login_shell '/bin/false' }
+  it { should belong_to_primary_group 'prometheus' }
 end
 
-describe file('/opt/prometheus/prometheus.yml'), :if => os[:family] == 'ubuntu' do
+describe file('/usr/local/bin/prometheus'), :if => os[:family] == 'ubuntu' do
   it { should be_file }
+  it { should be_owned_by 'prometheus' }
+end
+
+describe file('/usr/local/bin/promtool'), :if => os[:family] == 'ubuntu' do
+  it { should be_file }
+  it { should be_owned_by 'prometheus' }
+end
+
+describe file('/etc/prometheus'), :if => os[:family] == 'ubuntu' do
+  it { should be_directory }
+  it { should be_owned_by 'prometheus' }
+end
+
+describe file('/etc/prometheus/console_libraries'), :if => os[:family] == 'ubuntu' do
+  it { should be_directory }
+  it { should be_owned_by 'prometheus' }
+end
+
+describe file('/etc/prometheus/consoles'), :if => os[:family] == 'ubuntu' do
+  it { should be_directory }
+  it { should be_owned_by 'prometheus' }
+end
+
+describe file('/var/lib/prometheus'), :if => os[:family] == 'ubuntu' do
+  it { should be_directory }
+  it { should be_owned_by 'prometheus' }
+end
+
+describe file('/etc/prometheus/prometheus.yml'), :if => os[:family] == 'ubuntu' do
+  it { should be_file }
+  it { should be_owned_by 'prometheus' }
   it { should contain 'scrape_interval:     5s' }
   it { should contain 'evaluation_interval: 5s' }
   it { should contain 'node' }
   it { should contain 'localhost:9100' }
   it { should contain 'prometheus' }
   it { should contain 'localhost:9090' }
+  it { should contain 'alertmanager' }
+  it { should contain 'localhost:9093' }
 end
