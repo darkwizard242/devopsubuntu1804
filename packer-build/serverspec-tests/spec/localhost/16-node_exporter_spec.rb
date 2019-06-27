1734fae2c7aea8a61c4feaa93d572dec4c24a648
darkwizard242 <am900820@gmail.com>
2019-06-27 17:02:17 -0400

    refactored serverspec infra test for node_exporter

--- packer-build/serverspec-tests/spec/localhost/16-node_exporter_spec.rb
+++ packer-build/serverspec-tests/spec/localhost/16-node_exporter_spec.rb
@@ -9,6 +9,13 @@ describe port(9100), :if => os[:family] == 'ubuntu' do
   it { should be_listening }
 end
 
+describe user('node_exporter') do
+  it { should exist }
+  it { should have_login_shell '/bin/false' }
+  it { should belong_to_primary_group 'node_exporter' }
+end
+
 describe file('/usr/local/bin/node_exporter'), :if => os[:family] == 'ubuntu' do
   it { should be_file }
+  it { should be_owned_by 'node_exporter' }
 end
