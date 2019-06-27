9e7d0a20e3328f0146125e93dba5faaf9b6aa21e
darkwizard242 <am900820@gmail.com>
2019-06-27 17:03:07 -0400

    refactored serverspec infra test for grafana

--- packer-build/serverspec-tests/spec/localhost/18-grafana_spec.rb
+++ packer-build/serverspec-tests/spec/localhost/18-grafana_spec.rb
@@ -9,6 +9,37 @@ describe port(3000), :if => os[:family] == 'ubuntu' do
   it { should be_listening }
 end
 
-describe file('/usr/local/bin/grafana-server'), :if => os[:family] == 'ubuntu' do
+describe user('grafana') do
+  it { should exist }
+  it { should have_login_shell '/bin/false' }
+  it { should belong_to_primary_group 'grafana' }
+end
+
+describe file('/opt/grafana/bin/grafana-server'), :if => os[:family] == 'ubuntu' do
   it { should be_file }
+  it { should be_owned_by 'grafana' }
+end
+
+describe file('/opt/grafana'), :if => os[:family] == 'ubuntu' do
+  it { should be_directory }
+  it { should be_owned_by 'grafana' }
+end
+
+describe file('/var/lib/grafana'), :if => os[:family] == 'ubuntu' do
+  it { should be_directory }
+  it { should be_owned_by 'grafana' }
+end
+
+describe file('/var/log/grafana'), :if => os[:family] == 'ubuntu' do
+  it { should be_directory }
+  it { should be_owned_by 'grafana' }
+end
+
+describe file('/opt/grafana/conf/grafana.ini'), :if => os[:family] == 'ubuntu' do
+  it { should be_file }
+  it { should be_owned_by 'grafana' }
+  it { should contain 'data = /var/lib/grafana' }
+  it { should contain 'logs = /var/log/grafana' }
+  it { should contain 'plugins = /opt/grafana/plugins' }
+  it { should contain 'provisioning = /opt/grafana/conf/provisioning' }
 end
