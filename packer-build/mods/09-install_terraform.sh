3eb4acd34d45d3c0f5dfda4fb13d2db98ff74442
darkwizard242 <am900820@gmail.com>
2019-09-01 22:09:18 -0400

    terraform optimization and hellcheck fixes for: SC2086, SC2181, SC2006

--- packer-build/mods/09-install_terraform.sh
+++ packer-build/mods/09-install_terraform.sh
@@ -1,34 +1,35 @@
-#!/bin/bash -eu
+#!/bin/bash -e
 
+# Shellcheck fixes for: SC2086, SC2181, SC2006
 
 ## Installing packages required for Terraform
 dependencies="wget unzip"
 
 for dependency in $dependencies;
 do
-  dpkg -s $dependency &> /dev/null && echo -e
-  if [ $? -eq 0 ];
+  if dpkg -s "$dependency" &> /dev/null;
     then
-      echo "$dependency is already available and installed within the system." && echo -e
+      echo -e "\n$dependency is already available and installed within the system."
     else
-      echo "About to install $dependency." && echo -e
-      DEBIAN_FRONTEND=non-interactive apt-get install $dependency -y
+      echo -e "\nAbout to install:\t$dependency"
+      DEBIAN_FRONTEND=non-interactive apt-get install "$dependency" -y
   fi
 done
 
 
-## Download Terraform and extract binary to /bin/
+## Download Terraform and extract binary to /usr/local/bin/
 binary="terraform"
-version="0.12.0"
+version="0.12.7"
 osarch="linux_amd64"
-if [ -e /usr/local/bin/terraform ]
+extract_path="/usr/local/bin"
+if [ -e $extract_path/$binary ]
 then
-  ver_existing=`$binary --version`
-  echo "Terraform binary currently exists in /bin/ !"
-  echo "Currently available version is: $ver_existing"
+  ver_existing=$($binary --version)
+  echo -e "\n$binary currently exists in:\t$extract_path/$binary"
+  echo -e "\nCurrently available version is:\t$ver_existing"
 else
-  wget -v https://releases.hashicorp.com/${binary}/${version}/${binary}_${version}_${osarch}.zip
-  unzip ${binary}_${version}_${osarch}.zip -d /bin/ && rm -r ${binary}_${version}_${osarch}.zip
-  ver_fresh=`$binary --version`
-  echo "Installed version is: $ver_fresh"
+  wget -v -O /tmp/${binary}.zip https://releases.hashicorp.com/${binary}/${version}/${binary}_${version}_${osarch}.zip
+  unzip /tmp/${binary}.zip -d $extract_path && rm -rv /tmp/${binary}.zip
+  ver_fresh=$($binary --version)
+  echo -e "\nInstalled version is: $ver_fresh"
 fi
