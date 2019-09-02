389195d84a76c37c14bf6662172173c381bee814
darkwizard242 <am900820@gmail.com>
2019-09-01 22:07:13 -0400

    python3,  pip3 optimization and Shellcheck fixes for: SC2181

--- packer-build/mods/05-install_python3_pip3.sh
+++ packer-build/mods/05-install_python3_pip3.sh
@@ -1,26 +1,28 @@
-#!/bin/bash -eu
+#!/bin/bash -e
+
+# Shellcheck fixes for: SC2086, SC2181, SC2006
 
 ## Install python3 and python3-pip
 packages="python3 python3-pip"
 
 for package in $packages;
 do
-  dpkg -s $package &> /dev/null && echo -e
-  if [ $? -eq 0 ];
+  if dpkg -s "$package" &> /dev/null;
     then
-      echo "$package is already available and installed within the system" && echo -e
+      echo -e "\n$package is already available and installed within the system."
     else
-      echo "About to install $package" && echo -e
-      DEBIAN_FRONTEND=non-interactive apt-get install $package -y
+      echo -e "\nAbout to install: $package"
+      DEBIAN_FRONTEND=non-interactive apt-get install "$package" -y
   fi
 done
 
 
 ## Upgrade pip for python3
-pip3_ver=`python3 -m pip --version`
-if [ $? -eq 0 ];
+pip3_ver=$(python3 -m pip --version)
+
+if which pip3;
 then
-  echo "pip for python3 is installed and is: $pip3_ver . Going to upgrade it (if candidate is available)." 
+  echo -e "\npip for python3 is installed and is:\t$pip3_ver . Going to upgrade it (if candidate is available).\n"
   python3 -m pip install -U pip
 else
   echo "pip for python3 is not available."
