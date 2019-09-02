ce4d49988a77672af46c512752bb49da1e807a93
darkwizard242 <am900820@gmail.com>
2019-09-01 22:12:16 -0400

    node_exporter optimization and Shellcheck fixes for: SC2181, SC2034, SC2086, SC2006, SC2164

--- packer-build/mods/16-install_node_exporter.sh
+++ packer-build/mods/16-install_node_exporter.sh
@@ -1,58 +1,58 @@
-#!/bin/bash
+#!/bin/bash -e
+
+# Shellcheck fixes for: SC2181, SC2034, SC2086, SC2006, SC2164
 
 ## Installing packages required for node_exporter
 dependencies="wget tar"
 
 for dependency in $dependencies;
 do
-  dpkg -s $dependency &> /dev/null && echo -e
-  if [ $? -eq 0 ];
+  if dpkg -s "$dependency" &> /dev/null;
     then
-      echo "$dependency is already available and installed within the system." && echo -e
+      echo -e "\n$dependency is already available and installed within the system."
     else
-      echo "About to install $dependency." && echo -e
-      DEBIAN_FRONTEND=non-interactive apt-get install $dependency -y
+      echo -e "About to install:\t$dependency."
+      DEBIAN_FRONTEND=non-interactive apt-get install "$dependency" -y
   fi
 done
 
 
-## Download Node Exporter to support node_exporter and extract binary to /bin/
+## Download Node Exporter to support node_exporter and extract binary to /usr/local/bin
 binary="node_exporter"
 version="0.18.1"
 osarch="linux-amd64"
-if [ -e /usr/local/bin/node_exporter ]
+extract_path="/usr/local/bin"
+if [ -e $extract_path/$binary ]
 then
-  ver_existing=`$binary --version`
-  echo "node_exporter binary currently exists in /bin/ !"
+  echo -e "\n$binary currently exists in:\t$extract_path"
 else
-  id $binary &> /dev/null && echo -e
-  if [ $? -eq 0 ];
+  if id $binary &> /dev/null;
     then
-      echo "The user: $binary does exist." && echo -e
+      echo -e "\nThe user:\t$binary\tdoes exist.\n"
     else
-      echo "The user: $binary does not exist. Creating user: $binary !"
+      echo -e "\nThe user:\t$binary\tdoesn't exist. Creating user:\t$binary\t"
       useradd --no-create-home --shell /bin/false $binary
   fi
-  echo -e "\nInstalling ${binary}!\n"
-  cd /opt/
-  wget -v https://github.com/prometheus/${binary}/releases/download/v${version}/${binary}-${version}.${osarch}.tar.gz
   if [ -d "/opt/$binary" ];
   then
-    echo -e "\nDirectory: /opt/$binary exists. About to remove it.\n"
+    echo -e "\nDirectory:\t/opt/$binary\texists. About to remove it.\n"
     rm -rfv /opt/$binary
   else
-    echo -e "\nDirectory: /opt/$binary doesn't exist. Creating /opt/$binary !\n"
+    echo -e "\nDirectory:\t/opt/$binary\tdoesn't exist. Creating:\t/opt/$binary\t\n"
     mkdir -pv /opt/$binary
   fi
-  tar -xzf ${binary}-${version}.${osarch}.tar.gz -C /opt/$binary --strip-components=1 && rm -r ${binary}-${version}.${osarch}.tar.gz
-  cd /opt/${binary}
-  echo -e "\nMoving binary files to /usr/local/bin\n"
-  mv -v /opt/${binary}/${binary} /usr/local/bin/
-  echo -e "\nAssigning ownership of /usr/local/bin/${binary}\n"
-  chown -Rv ${binary}:${binary} /usr/local/bin/${binary}
-  echo -e "\nInstalled version is: $version"
+  echo -e "\nInstalling:\t${binary}!\n"
+  wget -v -O /tmp/${binary}.tar.gz https://github.com/prometheus/${binary}/releases/download/v${version}/${binary}-${version}.${osarch}.tar.gz
+  echo -e "\nExtracting: /tmp/${binary}.tar.gz \t to:\t /opt/$binary"
+  tar -xzf /tmp/${binary}.tar.gz -C /opt/$binary --strip-components=1
+  echo -e "\nRemoving:\t/tmp/${binary}.tar.gz" && rm -rv /tmp/${binary}.tar.gz
+  echo -e "\nMoving binary files to:\t$extract_path\n"
+  mv -v /opt/${binary}/${binary} $extract_path/
+  echo -e "\nAssigning ownership of:\t/$extract_path/${binary}\n"
+  chown -Rv ${binary}:${binary} $extract_path/${binary}
+  echo -e "\nInstalled version is:\t$version\n"
   rm -rfv /opt/${binary}
-  echo -e "\nCreating service for node_exporter"
+  echo -e "\nCreating service for:\t$binary"
   cat <<EOF >/etc/systemd/system/${binary}.service
 [Unit]
 Description=Node_Exporter - Exporter for machine metrics.
@@ -62,16 +62,15 @@ After=network.target
 User=${binary}
 Group=${binary}
 Type=simple
-ExecStart=/usr/local/bin/${binary}
+ExecStart=/$extract_path/${binary}
 
 [Install]
 WantedBy=multi-user.target
 EOF
   echo -e "\nPerforming systemctl daemon reload."
   systemctl daemon-reload
-  echo -e "\nEnabling systemctl service for ${binary}."
+  echo -e "\nEnabling systemctl service for:\t${binary}"
   systemctl enable ${binary}.service
-  echo -e "\nStarting systemctl service for ${binary}.\n"
+  echo -e "\nStarting systemctl service for:\t${binary}"
   systemctl start ${binary}.service
-  echo -e "\n"
 fi
