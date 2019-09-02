40c8bda1e7715cf854098667695171ae31c8a9ac
darkwizard242 <am900820@gmail.com>
2019-09-01 22:14:05 -0400

    aws-vault optimization and Shellcheck fixes for: SC2181, SC2006, SC2140

--- packer-build/mods/20-install_aws-vault.sh
+++ packer-build/mods/20-install_aws-vault.sh
@@ -1,35 +1,35 @@
 #!/bin/bash -eu
 
+# Shellcheck fixes for: SC2181, SC2006, SC2140
 
-## Installing packages required for Terraform
+## Installing packages required for aws-vault
 dependencies="curl"
 
 for dependency in $dependencies;
 do
-  dpkg -s $dependency &> /dev/null && echo -e
-  if [ $? -eq 0 ];
+  if dpkg -s $dependency &> /dev/null;
     then
-      echo "$dependency is already available and installed within the system." && echo -e
+      echo -e "$dependency is already available and installed within the system."
     else
-      echo "About to install $dependency." && echo -e
-      DEBIAN_FRONTEND=non-interactive apt-get install $dependency -y
+      echo -e "\nAbout to install:\t$dependency\n"
+      DEBIAN_FRONTEND=non-interactive apt-get install "$dependency" -y
   fi
 done
 
 
-## Download Terraform and extract binary to /bin/
+## Download aws-vault and extract binary to /bin/
 binary="aws-vault"
 version="4.6.2"
 osarch="linux-amd64"
 if [ -e /usr/local/bin/aws-vault ]
 then
-  ver_existing=`$binary --version`
-  echo "Terraform binary currently exists in /bin/ !"
-  echo "Currently available version is: $ver_existing"
+  ver_existing=$(binary --version)
+  echo -e "\n$binary currently exists in:\t/usr/local/bin/"
+  echo -e "\nCurrently available version is:\t$ver_existing"
 else
   curl -L -o /usr/local/bin/aws-vault https://github.com/99designs/${binary}/releases/download/v${version}/${binary}-${osarch}
-  echo -e "\n\nexport AWS_VAULT_BACKEND="file"" >> .bashrc
+  echo -e "\n\nexport AWS_VAULT_BACKEND=\"file\"" >> .bashrc
   chmod -v 0755 /usr/local/bin/${binary}
-  ver_fresh=`$binary --version`
-  echo "Installed version is: $ver_fresh"
+  ver_fresh=$(binary --version)
+  echo -e "\nInstalled version is:\t$ver_fresh"
 fi
