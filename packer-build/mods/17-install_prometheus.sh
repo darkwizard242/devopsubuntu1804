5b5a10fd22964bbd3a7a79c82864fce48fe19f79
darkwizard242 <am900820@gmail.com>
2019-09-01 22:12:41 -0400

    prometheus optimization and Shellcheck fixes for: SC2181, SC2086, SC2164

--- packer-build/mods/17-install_prometheus.sh
+++ packer-build/mods/17-install_prometheus.sh
@@ -1,52 +1,54 @@
 #!/bin/bash
 
+# Shellcheck fixes for: SC2181, SC2086, SC2164
+
 ## Installing packages required for Prometheus
 dependencies="wget tar"
 
 for dependency in $dependencies;
 do
-  dpkg -s $dependency &> /dev/null && echo -e
-  if [ $? -eq 0 ];
+  if dpkg -s "$dependency" &> /dev/null;
     then
-      echo "$dependency is already available and installed within the system." && echo -e
+      echo -e "\n$dependency is already available and installed within the system."
     else
-      echo "About to install $dependency." && echo -e
-      DEBIAN_FRONTEND=non-interactive apt-get install $dependency -y
+      echo -e "\nAbout to install:\t$dependency."
+      DEBIAN_FRONTEND=non-interactive apt-get install "$dependency" -y
   fi
 done
 
 
-## Download Prometheus and extract binary to /bin/
+## Download Prometheus and extract binary to /usr/local/bin/
 binary="prometheus"
 version="2.10.0"
 osarch="linux-amd64"
-if [ -e /usr/local/bin/prometheus ]
+extract_path="/usr/local/bin"
+if [ -e $extract_path/$binary ]
 then
-  echo "Prometheus binary currently exists in /bin/ !"
+  ver_existing=$($binary --version)
+  echo -e "\n$binary currently exists in:\t$extract_path"
 else
-  id $binary &> /dev/null && echo -e
-  if [ $? -eq 0 ];
+  if id $binary &> /dev/null;
     then
-      echo "The user: $binary does exist." && echo -e
+      echo -e "\nThe user:\t$binary does exist.\n"
     else
-      echo "The user: $binary does not exist. Creating user: $binary !"
+      echo -e "The user: $binary does not exist. Creating user:\t$binary\n"
       useradd --no-create-home --shell /bin/false $binary
   fi
   mkdir -pv /etc/$binary /var/lib/$binary
   chown -v $binary:$binary /var/lib/$binary
-  echo -e "\nInstalling ${binary}!\n"
-  cd /opt/
-  wget -v https://github.com/${binary}/${binary}/releases/download/v${version}/${binary}-${version}.${osarch}.tar.gz
+  echo -e "\nInstalling:\t${binary}"
   if [ -d "/opt/$binary" ];
   then
-    echo -e "Directory: /opt/$binary exists. About to remove it."
+    echo -e "\nDirectory:\t/opt/$binary exists. About to remove it.\n"
     rm -rfv /opt/$binary
   else
-    echo -e "\nDirectory: /opt/$binary doesn't exist. Creating /opt/$binary !\n"
+    echo -e "\nDirectory:\t/opt/$binary doesn't exist. Creating: \t/opt/$binary\n"
     mkdir -pv /opt/$binary
   fi
-  tar -xzf ${binary}-${version}.${osarch}.tar.gz -C /opt/$binary --strip-components=1 && rm -r ${binary}-${version}.${osarch}.tar.gz
-  cd /opt/${binary}
+  wget -v -O /tmp/${binary}.tar.gz https://github.com/${binary}/${binary}/releases/download/v${version}/${binary}-${version}.${osarch}.tar.gz
+  echo -e "\nExtracting: /tmp/${binary}.tar.gz \t to:\t /opt/$binary"
+  tar -xzf /tmp/${binary}.tar.gz -C /opt/$binary --strip-components=1
+  echo -e "\nRemoving: /tmp/${binary}.tar.gz" && rm -rv /tmp/${binary}.tar.gz
   rm -v /opt/${binary}/${binary}.yml
   cat <<EOF >/opt/${binary}/${binary}.yml
 global:
@@ -76,19 +78,18 @@ scrape_configs:
   #   static_configs:
   #   - targets: ['localhost:8000']
 EOF
-  echo -e "\nMoving binary files to /usr/local/bin\n"
-  mv -v /opt/${binary}/${binary} /opt/${binary}/promtool /usr/local/bin/
+  echo -e "\nMoving binary files to $extract_path\n"
+  mv -v /opt/${binary}/${binary} /opt/${binary}/promtool $extract_path/
   echo -e "\nMoving console dir(s) & ${binary}.yml /etc/${binary}\n"
   mv -v /opt/${binary}/console* /opt/${binary}/${binary}.yml /etc/${binary}/
-  echo -e "\nAssigning ownership of /usr/local/bin/${binary} & /usr/local/bin/promtool to ${binary}\n"
-  chown -Rv ${binary}:${binary} /usr/local/bin/${binary} /usr/local/bin/promtool
+  echo -e "\nAssigning ownership of $extract_path/${binary} & /usr/local/bin/promtool to ${binary}\n"
+  chown -Rv ${binary}:${binary} $extract_path/${binary} $extract_path/promtool
   echo -e "\nAssigning ownership of /etc/${binary} to ${binary}\n"
   chown -Rv ${binary}:${binary} /etc/${binary}
   echo -e "\nAssigning ownership of /var/lib/${binary} to ${binary}\n"
   chown -Rv ${binary}:${binary} /var/lib/${binary}
   echo -e "\nRemoving temporary ${binary} directory from /opt"
   rm -rfv /opt/${binary}
-  echo -e "\nInstalled version is: $version"
   cat <<EOF >/etc/systemd/system/${binary}.service
 [Unit]
 Description=Prometheus - monitoring system and time series database.
@@ -99,7 +100,7 @@ After=network-online.target
 User=${binary}
 Group=${binary}
 Type=simple
-ExecStart=/usr/local/bin/${binary} \
+ExecStart=$extract_path/${binary} \
     --config.file /etc/${binary}/${binary}.yml \
     --storage.tsdb.path /var/lib/${binary}/ \
     --web.console.templates=/etc/${binary}/consoles \
@@ -109,13 +110,10 @@ ExecStart=/usr/local/bin/${binary} \
 WantedBy=multi-user.target
 
 EOF
-  echo -e "\nPerforming systemctl daemon reload.\n"
+  echo -e "\nPerforming systemctl daemon reload."
   systemctl daemon-reload
-  echo -e "\nEnabling systemctl service for ${binary}.\n"
+  echo -e "\nEnabling systemctl service for:\t${binary}"
   systemctl enable ${binary}.service
-  echo -e "\nStarting systemctl service for ${binary}.\n"
+  echo -e "\nStarting systemctl service for:\t${binary}"
   systemctl start ${binary}.service
-  echo -e "\n"
-  #echo -e "\nExecuting PROMETHEUS now!\n!!"
-  #/bin/prometheus --config.file=/opt/${binary}-${version}.${osarch}/${binary}.yml > /dev/null 2>&1 &
 fi
