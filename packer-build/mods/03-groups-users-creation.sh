7cf35f73170efc802e64fe3d62e6cfbc81d6c8ea
darkwizard242 <am900820@gmail.com>
2019-09-01 22:06:11 -0400

    user/group optimization  and Shellcheck fixes for: SC2196, SC2181, SC2086, SC2046

--- packer-build/mods/03-groups-users-creation.sh
+++ packer-build/mods/03-groups-users-creation.sh
@@ -1,17 +1,18 @@
 #!/bin/bash -eu
 
 
+# Shellcheck fixes for: SC2196, SC2181, SC2086, SC2046
+
 ## Create groups.
 listOfGroups="docker jenkins ansible"
 for groupname in $listOfGroups
 do
-        egrep -i "^$groupname" /etc/group &> /dev/null
-        if [ $? -eq 0 ];
+  if grep -Ei "^$groupname" /etc/group &> /dev/null;
     then
-      echo "The group: $groupname does exist." && echo -e
+      echo -e "\nThe group: $groupname does exist."
     else
-      echo "The group: $groupname doesn't exist. Creating group: $groupname !" && echo -e
-      groupadd -r $groupname
+      echo -e "\nThe group: $groupname doesn't exist. Creating group:\t$groupname"
+      groupadd -r "$groupname"
   fi
 done
 
@@ -19,16 +20,14 @@ done
 listOfUsers="docker ansible"
 for username in $listOfUsers;
 do
-  id $username &> /dev/null && echo -e
-  if [ $? -eq 0 ];
+  if id "$username" &> /dev/null;
     then
-      echo "The user: $username does exist." && echo -e
-      usermod -aG docker $username
+      echo -e "\nThe user: $username does exist."
+      usermod -aG docker "$username"
     else
-      echo "The user: $username does not exist. Creating user: $username !"
-      useradd $username -m -s /bin/bash -p $(openssl passwd -1 password) -g $username
-      echo -e "Adding user: $username to sudo group." && usermod -aG sudo $username
-      echo -e "Adding user: $username to docker group." && usermod -aG docker $username
-      echo -e
+      echo -e "\nThe user: $username does not exist. Creating user: $username !"
+      useradd "$username" -m -s /bin/bash -p "$(openssl passwd -1 password)" -g "$username"
+      echo -e "\nAdding user: $username to sudo group." && usermod -aG sudo "$username"
+      echo -e "\nAdding user: $username to docker group." && usermod -aG docker "$username"
   fi
 done
