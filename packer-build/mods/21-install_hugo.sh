5c17884117be4874629854fb18fcc7c19b974ddc
darkwizard242 <am900820@gmail.com>
2019-09-01 22:14:33 -0400

    hugo optimization and Shellcheck fixes for: SC2181, SC2006

--- packer-build/mods/21-install_hugo.sh
+++ packer-build/mods/21-install_hugo.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
+# Shellcheck fixes for: SC2181, SC2006
 
 ## Installing packages required for Hugo
 dependencies="wget"
 
 for dependency in $dependencies;
 do
-  dpkg -s $dependency &> /dev/null && echo -e
-  if [ $? -eq 0 ];
+  if dpkg -s "$dependency" &> /dev/null;
     then
-      echo "$dependency is already available and installed within the system." && echo -e
+      echo -e "\n$dependency is already available and installed within the system."
     else
-      echo "About to install $dependency." && echo -e
-      DEBIAN_FRONTEND=non-interactive apt-get install $dependency -y
+      echo -e "\nAbout to install:\t$dependency"
+      DEBIAN_FRONTEND=non-interactive apt-get install "$dependency" -y
   fi
 done
 
@@ -21,15 +21,16 @@ done
 binary="hugo"
 version="0.56.0"
 osarch="Linux-64bit"
-if [ -e /usr/local/bin/hugo ]
+extract_path="/usr/local/bin"
+if [ -e $extract_path/$binary ]
 then
-  ver_existing=`$binary --version`
-  echo -e "\n${binary} binary currently exists in /bin/ !"
-  echo -e "\nCurrently available version is: $ver_existing"
+  ver_existing=$($binary --version)
+  echo -e "\n$binary currently exists in:\t$extract_path/$binary"
+  echo -e "\nCurrently available version is:\t$ver_existing"
 else
   wget -v -O /tmp/${binary}.tar.gz https://github.com/gohugoio/${binary}/releases/download/v${version}/${binary}_${version}_${osarch}.tar.gz
   tar -zxf /tmp/${binary}.tar.gz -C /usr/local/bin ${binary} && rm -rv /tmp/${binary}.tar.gz
-  ver_fresh=`$binary version | awk '{print $5}'`
-  echo -e "\nInstalled version of ${binary} is: $ver_fresh\n"
+  ver_fresh=$(binary version | awk '{print $5}')
+  echo -e "\nInstalled version of ${binary} is:\t$ver_fresh\n"
 fi
 
