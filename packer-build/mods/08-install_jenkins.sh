c57878ead6f18f9e227e1ed41de8726a8ec5e040
darkwizard242 <am900820@gmail.com>
2019-09-01 22:08:31 -0400

    jenkins optimization and Shellcheck fixes for: SC2181

--- packer-build/mods/08-install_jenkins.sh
+++ packer-build/mods/08-install_jenkins.sh
@@ -1,32 +1,30 @@
-#!/bin/bash -eu
+#!/bin/bash -e
 
+# Shellcheck fixes for: SC2181
 
 ## Install jenkins
 package="jenkins"
 
-dpkg -s $package &> /dev/null && echo -e
-if [ $? -eq 0 ];
+if dpkg -s $package &> /dev/null;
 then
-  echo "$package is already available and installed within the system" && echo -e
+  echo -e "\n$package is already available and installed within the system."
 else
-  echo "About to install $package" && echo -e
+  echo -e "\nAbout to install:\t$package"
   wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-  sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-  DEBIAN_FRONTEND=non-interactive apt-get update -y
+  echo "deb http://pkg.jenkins.io/debian-stable binary/" > /etc/apt/sources.list.d/jenkins.list
+  DEBIAN_FRONTEND=non-interactive apt-get update
   DEBIAN_FRONTEND=non-interactive apt-get install $package -y
   systemctl enable $package && systemctl start $package
 fi
 
 
 ## Check service status of jenkins
-systemctl status $package --no-pager -l
-if [ $? -eq 0 ];
+if systemctl status $package --no-pager -l;
 then
   echo -e && echo "$package is UP & Running"
 else
   echo -e && echo "Service status for $package returned an error"
 fi
 
-echo -e "Adding user: jenkins to sudo group." && usermod -aG sudo jenkins
-echo -e "Adding user: jenkins to docker group." && usermod -aG docker jenkins
-echo -e
+echo -e "\nAdding user: jenkins to sudo group." && usermod -aG sudo jenkins
+echo -e "\nAdding user: jenkins to docker group." && usermod -aG docker jenkins
