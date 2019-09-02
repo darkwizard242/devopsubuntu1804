17292864db96d76e9841670161c3dd6e100d9e16
darkwizard242 <am900820@gmail.com>
2019-09-01 22:13:35 -0400

    Shellcheck fixes for: SC2181, SC2086, SC2164, SC2028, SC2034, SC2161

--- packer-build/mods/19-install_alertmanager.sh
+++ packer-build/mods/19-install_alertmanager.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
 
+# Shellcheck fixes for: SC2181, SC2086, SC2164, SC2028, SC2034, SC2161
+
 ## Installing packages required for alertmanager
 dependencies="wget tar"
 
 for dependency in $dependencies;
 do
-  dpkg -s $dependency &> /dev/null && echo -e
-  if [ $? -eq 0 ];
+  if dpkg -s "$dependency" &> /dev/null;
     then
-      echo "$dependency is already available and installed within the system." && echo -e
+      echo -e "\n$dependency is already available and installed within the system."
     else
-      echo "About to install $dependency." && echo -e
-      DEBIAN_FRONTEND=non-interactive apt-get install $dependency -y
+      echo -e "About to install:\t$dependency."
+      DEBIAN_FRONTEND=non-interactive apt-get install "$dependency" -y
   fi
 done
 
 
-## Download Node Exporter to support alertmanager and extract binary to /bin/
+## Download AlertManager to support alertmanager and extract binary to /bin/
 binary="alertmanager"
 version="0.17.0"
 osarch="linux-amd64"
-if [ -e /usr/local/bin/alertmanager ]
+extract_path="/usr/local/bin"
+if [ -e $extract_path/$binary ]
 then
-  ver_existing=`$binary --version`
-  echo "alertmanager binary currently exists in /bin/ !"
+  ver_existing=$($binary --version)
+  echo -e "\n$binary currently exists in:\t$extract_path"
 else
-  id $binary &> /dev/null && echo -e
-  if [ $? -eq 0 ];
+  if id $binary &> /dev/null;
     then
-      echo "The user: $binary does exist." && echo -e
+      echo -e "\nThe user:\t$binary\tdoes exist.\n"
     else
-      echo "The user: $binary does not exist. Creating user: $binary !"
+      echo -e "\nThe user:\t$binary\tdoesn't exist. Creating user:\t$binary\t"
       useradd --no-create-home --shell /bin/false $binary
   fi
   mkdir -pv /etc/$binary
-  echo -e "\nInstalling ${binary}!\n"
-  cd /opt/
-  wget -v https://github.com/prometheus/${binary}/releases/download/v${version}/${binary}-${version}.${osarch}.tar.gz
+  echo -e "\nInstalling:\t${binary}!\n"
+  wget -v -O /tmp/${binary}.tar.gz https://github.com/prometheus/${binary}/releases/download/v${version}/${binary}-${version}.${osarch}.tar.gz
   if [ -d "/opt/$binary" ];
   then
     echo -e "\nDirectory: /opt/$binary exists. About to remove it.\n"
@@ -45,19 +45,19 @@ else
     echo -e "\nDirectory: /opt/$binary doesn't exist. Creating /opt/$binary !\n"
     mkdir -pv /opt/$binary
   fi
-  tar -xzf ${binary}-${version}.${osarch}.tar.gz -C /opt/$binary --strip-components=1 && rm -r ${binary}-${version}.${osarch}.tar.gz
-  cd /opt/${binary}
-  echo -e "\nMoving binary files to /usr/local/bin\n"
-  mv -v /opt/${binary}/${binary} /opt/${binary}/amtool /usr/local/bin/
+  echo -e "\nExtracting: /tmp/${binary}.tar.gz \t to:\t /opt/$binary"
+  tar -xzf /tmp/${binary}.tar.gz -C /opt/$binary --strip-components=1--strip-components=1
+  echo -e "\nRemoving:\t/tmp/${binary}.tar.gz" && rm -rv /tmp/${binary}.tar.gz
+  echo -e "\nMoving binary files to $extract_path\n"
+  mv -v /opt/${binary}/${binary} /opt/${binary}/amtool $extract_path
   echo -e "\nMoving console dir(s) & ${binary}.yml /etc/${binary}\n"
   mv -v /opt/${binary}/${binary}.yml /etc/${binary}/
-  echo -e "\nAssigning ownership of /usr/local/bin/${binary} & /usr/local/bin/promtool to ${binary}\n"
+  echo -e "\nAssigning ownership of $extract_path/${binary} & $extract_path/promtool to ${binary}\n"
   chown -Rv ${binary}:${binary} /usr/local/bin/${binary} /usr/local/bin/amtool
   echo -e "\nAssigning ownership of /etc/${binary} to ${binary}\n"
   chown -Rv ${binary}:${binary} /etc/${binary}
   echo -e "\nRemoving temporary ${binary} directory from /opt"
   rm -rfv /opt/${binary}
-  echo -e "\nInstalled version is: $version"
   echo -e "\nCreating service for ${binary}"
   cat <<EOF >/etc/systemd/system/${binary}.service
 [Unit]
@@ -83,5 +83,4 @@ EOF
   systemctl enable ${binary}.service
   echo -e "\nStarting systemctl service for ${binary}.\n"
   systemctl start ${binary}.service
-  echo -e "\n"
 fi
