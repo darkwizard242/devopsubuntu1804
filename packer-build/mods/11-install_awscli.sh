cfa05dc59121c74b9610519a7c544124329facb2
darkwizard242 <am900820@gmail.com>
2019-09-01 22:10:06 -0400

    awscli optimization and # Shellcheck fixes for: SC2181

--- packer-build/mods/11-install_awscli.sh
+++ packer-build/mods/11-install_awscli.sh
@@ -1,11 +1,13 @@
-#!/bin/bash -eu
+#!/bin/bash -e
+
+# Shellcheck fixes for: SC2181
 
 # Check if python3-pip is install. If installed, then install aws-cli
-apt list --installed | grep python3-pip
-if [ $? -eq 0 ];
+
+if which pip3;
 then
-	echo "python3-pip is installed. Ready to install AWS CLI!"
+	echo -e "\npip3 is installed. Ready to install: AWS CLI!\n"
 	python3 -m pip install awscli
 else
-	echo "python3-pip is not installed. It is a pre-requisite for AWS CLI."
+	echo -e "\npython3-pip is not installed. It is a pre-requisite for AWS CLI.\n"
 fi
