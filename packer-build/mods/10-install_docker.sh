8f8cd1cf29f04ee4996eeaf6f52797893c75e8f7
darkwizard242 <am900820@gmail.com>
2019-09-01 22:09:42 -0400

    docker optimization and Shellcheck fixes for: SC2086, SC2181, SC2006

--- packer-build/mods/10-install_docker.sh
+++ packer-build/mods/10-install_docker.sh
@@ -1,43 +1,46 @@
-#!/bin/bash -eu
+#!/bin/bash -e
 
+# Shellcheck fixes for: SC2086, SC2181, SC2006
 
 ## Installing packages required for docker-ce
 dependencies="apt-transport-https ca-certificates curl software-properties-common"
 
 for dependency in $dependencies;
 do
-  dpkg -s $dependency &> /dev/null && echo -e
-  if [ $? -eq 0 ];
+  if dpkg -s "$dependency" &> /dev/null;
     then
-      echo "$dependency is already available and installed within the system." && echo -e
+      echo -e "\n$dependency is already available and installed within the system."
     else
-      echo "About to install $dependency." && echo -e
-      DEBIAN_FRONTEND=non-interactive apt-get install $dependency -y
+      echo -e "\nAbout to install:\t$dependency"
+      DEBIAN_FRONTEND=non-interactive apt-get install "$dependency" -y
   fi
 done
 
 
 ## Installing docker-ce
+system_rel=$(lsb_release -cs)
 package="docker-ce"
-dpkg -s $package &> /dev/null && echo -e
-if [ $? -eq 0 ];
+if dpkg -s $package &> /dev/null;
   then
-    echo "$package is already available and installed within the system." && echo -e
+    echo -e "\n$package is already available and installed within the system."
   else
-    echo "About to install $package." && echo -e
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-    DEBIAN_FRONTEND=non-interactive apt-get update -y
+    echo -e "\nAbout to install:\t$package."
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+    echo -e "\nCreating docker repo file."
+    echo -e "deb [arch=amd64] https://download.docker.com/linux/ubuntu $system_rel stable"  > /etc/apt/sources.list.d/docker.list
+    DEBIAN_FRONTEND=non-interactive apt-get update
     DEBIAN_FRONTEND=non-interactive apt-get install $package -y
-    systemctl enable docker && systemctl start docker
+    echo -e "\nEnabling and starting docker service." && systemctl enable docker && systemctl start docker
 fi
 
 
 ## Verify if docker is working
-dockver=`docker --version`
-if [ $? -eq 0 ];
+dockver=$(docker --version)
+
+if docker --version;
   then
-    echo "Exit status for $package command returned back with a successful exit code." && echo "Installed version is: $dockver"
+    echo -e "\nExit status for $package command returned back with a successful exit code."
+    echo -e "\nInstalled version is:\t$dockver"
   else
-    echo "There was an issue with the execution of $package command." && echo -e
+    echo -e "\nThere was an issue with the execution of $package command."
 fi
