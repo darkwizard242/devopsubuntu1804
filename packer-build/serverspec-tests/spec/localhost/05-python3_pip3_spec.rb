fc5e04dab0a86fed5fd08312f5d31605f6c79726
darkwizard242 <am900820@gmail.com>
2019-10-05 00:10:45 -0400

    python3 pip serverspec updated

--- packer-build/serverspec-tests/spec/localhost/05-python3_pip3_spec.rb
+++ packer-build/serverspec-tests/spec/localhost/05-python3_pip3_spec.rb
@@ -1,19 +1,35 @@
 require 'spec_helper'
 
 ## Check for python3 package
-describe package('python3'), :if => os[:family] == 'ubuntu' do
-  it { should be_installed }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe package('python3') do
+      it { should be_installed }
+    end
+  end
 end
 
-describe command('python3 --version'), :if => os[:family] == 'ubuntu' do
-  its(:exit_status) { should eq 0 }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe command('python3 --version') do
+      its(:exit_status) { should eq 0 }
+    end
+  end
 end
 
 ## Check for python3-pip package
-describe package('python3-pip'), :if => os[:family] == 'ubuntu' do
-  it { should be_installed }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe package('python3-pip') do
+      it { should be_installed }
+    end
+  end
 end
 
-describe command('pip3 --version'), :if => os[:family] == 'ubuntu' do
-  its(:exit_status) { should eq 0 }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe command('pip3 --version') do
+      its(:exit_status) { should eq 0 }
+    end
+  end
 end
