f0cc2e2495317cd005bc308e9b12d6cc6aee0fe7
darkwizard242 <am900820@gmail.com>
2019-10-04 23:55:36 -0400

    vagrant init serverspec login updated

--- packer-build/serverspec-tests/spec/localhost/02-vagrant_init_spec.rb
+++ packer-build/serverspec-tests/spec/localhost/02-vagrant_init_spec.rb
@@ -1,10 +1,13 @@
 require 'spec_helper'
 
 ## Validate .ssh folder for vagrant.
-describe file('/home/vagrant/.ssh'), :if => os[:family] == 'ubuntu' do
-  it { should exist }
-  it { should be_directory }
-  it { should be_owned_by 'vagrant' }
-  it { should be_mode 700 }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe file('/home/vagrant/.ssh'), :if => os[:family] == 'ubuntu' do
+      it { should exist }
+      it { should be_directory }
+      it { should be_owned_by 'vagrant' }
+      it { should be_mode 700 }
+    end
+  end
 end
-
