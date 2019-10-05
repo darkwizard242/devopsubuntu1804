6088da5cabfa5331071eb2f4345a77bdc6e1d72b
darkwizard242 <am900820@gmail.com>
2019-10-04 23:25:43 -0400

    refactor serverspec rspec terraform test logic

--- packer-build/serverspec-tests/spec/localhost/09-terraform_spec.rb
+++ packer-build/serverspec-tests/spec/localhost/09-terraform_spec.rb
@@ -1,18 +1,26 @@
 require 'spec_helper'
 
 ## Check if terraform binary exists in /usr/local/bin and it's permissions
-describe file('/usr/local/bin/terraform'), :if => os[:family] == 'ubuntu' do
-  it { should exist }
-  it { should be_file }
-  it { should be_owned_by 'root' }
-  it { should be_readable.by_user('root') }
-  it { should be_readable.by('group') }
-  it { should be_executable.by_user('root') }
-  it { should be_executable.by('group') }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe file('/usr/local/bin/terraform') do
+      it { should exist }
+      it { should be_file }
+      it { should be_owned_by 'root' }
+      it { should be_readable.by_user('root') }
+      it { should be_readable.by('group') }
+      it { should be_executable.by_user('root') }
+      it { should be_executable.by('group') }
+    end
+  end
 end
 
 
 ## Check if terraform binary command exists with a successful exit code.
-describe command('terraform --version'), :if => os[:family] == 'ubuntu' do
-  its(:exit_status) { should eq 0 }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe command('terraform --version'), :if => os[:family] == 'ubuntu' do
+      its(:exit_status) { should eq 0 }
+    end
+  end
 end
