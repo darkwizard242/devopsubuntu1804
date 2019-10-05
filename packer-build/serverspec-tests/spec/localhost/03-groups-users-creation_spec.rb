c94e23b5656ac4fac75cc8b96228cc62767f9e7b
darkwizard242 <am900820@gmail.com>
2019-10-05 00:06:10 -0400

    groups and users serverspec updated

--- packer-build/serverspec-tests/spec/localhost/03-groups-users-creation_spec.rb
+++ packer-build/serverspec-tests/spec/localhost/03-groups-users-creation_spec.rb
@@ -1,45 +1,73 @@
 require 'spec_helper'
 
 ## Validate  groups.
-describe group('vagrant'), :if => os[:family] == 'ubuntu' do
-  it { should exist }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe group('vagrant') do
+      it { should exist }
+    end
+  end
 end
 
-describe group('docker'), :if => os[:family] == 'ubuntu' do
-  it { should exist }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe group('docker') do
+      it { should exist }
+    end
+  end
 end
 
-describe group('jenkins'), :if => os[:family] == 'ubuntu' do
-  it { should exist }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe group('jenkins') do
+      it { should exist }
+    end
+  end
 end
 
-describe group('ansible'), :if => os[:family] == 'ubuntu' do
-  it { should exist }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe group('ansible') do
+      it { should exist }
+    end
+  end
 end
 
 
 ## Validate users.
-describe user('vagrant'), :if => os[:family] == 'ubuntu' do
-  it { should exist }
-  it { should belong_to_group 'vagrant' }
-  it { should have_home_directory '/home/vagrant' }
-  it { should have_login_shell '/bin/bash' }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe user('vagrant') do
+      it { should exist }
+      it { should belong_to_group 'vagrant' }
+      it { should have_home_directory '/home/vagrant' }
+      it { should have_login_shell '/bin/bash' }
+    end
+  end
 end
 
-describe user('docker'), :if => os[:family] == 'ubuntu' do
-  it { should exist }
-  it { should belong_to_primary_group 'docker' }
-  it { should belong_to_group 'docker' }
-  it { should belong_to_group 'sudo' }
-  it { should have_home_directory '/home/docker' }
-  it { should have_login_shell '/bin/bash' }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe user('docker') do
+      it { should exist }
+      it { should belong_to_primary_group 'docker' }
+      it { should belong_to_group 'docker' }
+      it { should belong_to_group 'sudo' }
+      it { should have_home_directory '/home/docker' }
+      it { should have_login_shell '/bin/bash' }
+    end
+  end
 end
 
-describe user('ansible'), :if => os[:family] == 'ubuntu' do
-  it { should exist }
-  it { should belong_to_primary_group 'ansible' }
-  it { should belong_to_group 'docker' }
-  it { should belong_to_group 'sudo' }
-  it { should have_home_directory '/home/ansible' }
-  it { should have_login_shell '/bin/bash' }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe user('ansible') do
+      it { should exist }
+      it { should belong_to_primary_group 'ansible' }
+      it { should belong_to_group 'docker' }
+      it { should belong_to_group 'sudo' }
+      it { should have_home_directory '/home/ansible' }
+      it { should have_login_shell '/bin/bash' }
+    end
+  end
 end
