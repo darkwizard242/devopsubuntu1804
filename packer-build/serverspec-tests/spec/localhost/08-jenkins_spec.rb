edc7e8e4bf00f73fdd37ec848eb95ff45791236e
darkwizard242 <am900820@gmail.com>
2019-10-05 00:15:22 -0400

    jenkins serverspec updated

--- packer-build/serverspec-tests/spec/localhost/08-jenkins_spec.rb
+++ packer-build/serverspec-tests/spec/localhost/08-jenkins_spec.rb
@@ -1,24 +1,39 @@
 require 'spec_helper'
 
 ## Check if jenkins package is installed.
-describe package('jenkins'), :if => os[:family] == 'ubuntu' do
-  it { should be_installed }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe package('jenkins') do
+      it { should be_installed }
+    end
+  end
 end
 
 ## Check if jenkins service is enabled and running.
-describe service('jenkins'), :if => os[:family] == 'ubuntu' do
-  it { should be_enabled }
-  it { should be_running }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe service('jenkins') do
+      it { should be_enabled }
+      it { should be_running }
+    end
+  end
 end
 
 ## Check if listening on port 8080.
-describe port(8080), :if => os[:family] == 'ubuntu' do
-  it { should be_listening }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe port(8080) do
+      it { should be_listening }
+    end
+  end
 end
 
 ## Check if the jenkins directory exists and it's ownership.
-describe file('/var/lib/jenkins'), :if => os[:family] == 'ubuntu' do
-  it { should be_directory }
-  it { should be_owned_by 'jenkins' }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe file('/var/lib/jenkins') do
+      it { should be_directory }
+      it { should be_owned_by 'jenkins' }
+    end
+  end
 end
-
