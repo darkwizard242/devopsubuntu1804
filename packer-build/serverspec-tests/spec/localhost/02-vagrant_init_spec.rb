d4e21a7747686eca57aa861f08c651823304fa80
darkwizard242 <am900820@gmail.com>
2019-10-05 00:00:38 -0400

    removed redundant if/else

--- packer-build/serverspec-tests/spec/localhost/02-vagrant_init_spec.rb
+++ packer-build/serverspec-tests/spec/localhost/02-vagrant_init_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 ## Validate .ssh folder for vagrant.
 if os[:family] == 'ubuntu'
   if os[:release] == '18.04'
-    describe file('/home/vagrant/.ssh'), :if => os[:family] == 'ubuntu' do
+    describe file('/home/vagrant/.ssh') do
       it { should exist }
       it { should be_directory }
       it { should be_owned_by 'vagrant' }
