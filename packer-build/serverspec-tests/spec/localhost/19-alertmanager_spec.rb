f66590580999ca5de75dfc0c7e657831b3b0b9fb
darkwizard242 <am900820@gmail.com>
2019-10-05 00:34:23 -0400

    alertmanager serverspec updated

--- packer-build/serverspec-tests/spec/localhost/19-alertmanager_spec.rb
+++ packer-build/serverspec-tests/spec/localhost/19-alertmanager_spec.rb
@@ -1,36 +1,64 @@
 require 'spec_helper'
 
-describe service('alertmanager'), :if => os[:family] == 'ubuntu' do
-  it { should be_enabled }
-  it { should be_running.under('systemd') }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe service('alertmanager') do
+      it { should be_enabled }
+      it { should be_running.under('systemd') }
+    end
+  end
 end
 
-describe port(9093), :if => os[:family] == 'ubuntu' do
-  it { should be_listening }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe port(9093) do
+      it { should be_listening }
+    end
+  end
 end
 
-describe user('alertmanager') do
-  it { should exist }
-  it { should have_login_shell '/bin/false' }
-  it { should belong_to_primary_group 'alertmanager' }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe user('alertmanager') do
+      it { should exist }
+      it { should have_login_shell '/bin/false' }
+      it { should belong_to_primary_group 'alertmanager' }
+    end
+  end
 end
 
-describe file('/usr/local/bin/alertmanager'), :if => os[:family] == 'ubuntu' do
-  it { should be_file }
-  it { should be_owned_by 'alertmanager' }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe file('/usr/local/bin/alertmanager') do
+      it { should be_file }
+      it { should be_owned_by 'alertmanager' }
+    end
+  end
 end
 
-describe file('/usr/local/bin/amtool'), :if => os[:family] == 'ubuntu' do
-  it { should be_file }
-  it { should be_owned_by 'alertmanager' }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe file('/usr/local/bin/amtool') do
+      it { should be_file }
+      it { should be_owned_by 'alertmanager' }
+    end
+  end
 end
 
-describe file('/etc/alertmanager'), :if => os[:family] == 'ubuntu' do
-  it { should be_directory }
-  it { should be_owned_by 'alertmanager' }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe file('/etc/alertmanager') do
+      it { should be_directory }
+      it { should be_owned_by 'alertmanager' }
+    end
+  end
 end
 
-describe file('/etc/alertmanager/alertmanager.yml'), :if => os[:family] == 'ubuntu' do
-  it { should be_file }
-  it { should be_owned_by 'alertmanager' }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe file('/etc/alertmanager/alertmanager.yml') do
+      it { should be_file }
+      it { should be_owned_by 'alertmanager' }
+    end
+  end
 end
