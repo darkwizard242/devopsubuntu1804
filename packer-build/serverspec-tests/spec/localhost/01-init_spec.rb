408a3271f33e358257db1122155c8a2dbf5af04c
darkwizard242 <am900820@gmail.com>
2019-10-04 23:54:27 -0400

    init spec serverspec logic updated

--- packer-build/serverspec-tests/spec/localhost/01-init_spec.rb
+++ packer-build/serverspec-tests/spec/localhost/01-init_spec.rb
@@ -1,65 +1,117 @@
 require 'spec_helper'
 
 ## Check sudoers ownership, perms, file exists, contents.
-describe file('/etc/sudoers'), :if => os[:family] == 'ubuntu' do
-  it { should exist }
-  it { should be_file }
-  it { should be_owned_by 'root' }
-  it { should be_readable.by_user('root') }
-  it { should contain 'vagrant ALL=(ALL) NOPASSWD: ALL' }
-  it { should contain 'ansible ALL=(ALL) NOPASSWD: ALL' }
-  it { should contain 'jenkins ALL=(ALL) NOPASSWD: ALL' }
-  it { should contain 'docker ALL=(ALL) NOPASSWD: ALL' }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe file('/etc/sudoers') do
+      it { should exist }
+      it { should be_file }
+      it { should be_owned_by 'root' }
+      it { should be_readable.by_user('root') }
+      it { should contain 'vagrant ALL=(ALL) NOPASSWD: ALL' }
+      it { should contain 'ansible ALL=(ALL) NOPASSWD: ALL' }
+      it { should contain 'jenkins ALL=(ALL) NOPASSWD: ALL' }
+      it { should contain 'docker ALL=(ALL) NOPASSWD: ALL' }
+    end
+  end
 end
 
 ## Check for file to disable unattended updates - ownership, perms, file exists, contents.
-describe file('/etc/apt/apt.conf.d/10periodic'), :if => os[:family] == 'ubuntu' do
-  it { should exist }
-  it { should be_file }
-  it { should be_owned_by 'root' }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe file('/etc/apt/apt.conf.d/10periodic') do
+      it { should exist }
+      it { should be_file }
+      it { should be_owned_by 'root' }
+    end
+  end
 end
 
 ## Check whether the packages in 01-init.sh are available in the system
-describe package('build-essential'), :if => os[:family] == 'ubuntu' do
-  it { should be_installed }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe package('build-essential') do
+      it { should be_installed }
+    end
+  end
 end
 
-describe package('dkms'), :if => os[:family] == 'ubuntu' do
-  it { should be_installed }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe package('dkms') do
+      it { should be_installed }
+    end
+  end
 end
 
-describe package('nfs-common'), :if => os[:family] == 'ubuntu' do
-  it { should be_installed }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe package('nfs-common') do
+      it { should be_installed }
+    end
+  end
 end
 
-describe package('curl'), :if => os[:family] == 'ubuntu' do
-  it { should be_installed }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe package('curl') do
+      it { should be_installed }
+    end
+  end
 end
 
-describe package('wget'), :if => os[:family] == 'ubuntu' do
-  it { should be_installed }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe package('wget') do
+      it { should be_installed }
+    end
+  end
 end
 
-describe package('tmux'), :if => os[:family] == 'ubuntu' do
-  it { should be_installed }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe package('tmux') do
+      it { should be_installed }
+    end
+  end
 end
 
-describe package('xvfb'), :if => os[:family] == 'ubuntu' do
-  it { should be_installed }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe package('xvfb') do
+      it { should be_installed }
+    end
+  end
 end
 
-describe package('vim'), :if => os[:family] == 'ubuntu' do
-  it { should be_installed }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe package('vim') do
+      it { should be_installed }
+    end
+  end
 end
 
-describe package('inxi'), :if => os[:family] == 'ubuntu' do
-  it { should be_installed }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe package('inxi') do
+      it { should be_installed }
+    end
+  end
 end
 
-describe package('screenfetch'), :if => os[:family] == 'ubuntu' do
-  it { should be_installed }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe package('screenfetch') do
+      it { should be_installed }
+    end
+  end
 end
 
-describe package('tree'), :if => os[:family] == 'ubuntu' do
-  it { should be_installed }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe package('tree') do
+      it { should be_installed }
+    end
+  end
 end
