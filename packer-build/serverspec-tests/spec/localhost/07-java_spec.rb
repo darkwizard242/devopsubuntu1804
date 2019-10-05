4d612a5c4fb3d27a44a51f256fcbaa315c53ffd4
darkwizard242 <am900820@gmail.com>
2019-10-05 00:13:41 -0400

    java serverspec updated

--- packer-build/serverspec-tests/spec/localhost/07-java_spec.rb
+++ packer-build/serverspec-tests/spec/localhost/07-java_spec.rb
@@ -1,10 +1,17 @@
 require 'spec_helper'
 
 
-describe package('openjdk-8-jdk'), :if => os[:family] == 'ubuntu' do
-  it { should be_installed }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe package('openjdk-8-jdk') do
+    end
+  end
 end
 
-describe command('java -version'), :if => os[:family] == 'ubuntu' do
-  its(:exit_status) { should eq 0 }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe command('java -version') do
+      its(:exit_status) { should eq 0 }
+    end
+  end
 end
