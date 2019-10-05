a3e5b9e9c9561fe428a824902e1d79072af86e19
darkwizard242 <am900820@gmail.com>
2019-10-05 00:26:02 -0400

    prometheus serverspec updated

--- packer-build/serverspec-tests/spec/localhost/17-prometheus_spec.rb
+++ packer-build/serverspec-tests/spec/localhost/17-prometheus_spec.rb
@@ -1,59 +1,99 @@
 require 'spec_helper'
 
-describe service('prometheus'), :if => os[:family] == 'ubuntu' do
-  it { should be_enabled }
-  it { should be_running.under('systemd') }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe service('prometheus') do
+      it { should be_enabled }
+      it { should be_running.under('systemd') }
+    end
+  end
 end
 
-describe port(9090), :if => os[:family] == 'ubuntu' do
-  it { should be_listening }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe port(9090) do
+      it { should be_listening }
+    end
+  end
 end
 
-describe user('prometheus') do
-  it { should exist }
-  it { should have_login_shell '/bin/false' }
-  it { should belong_to_primary_group 'prometheus' }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe user('prometheus') do
+      it { should exist }
+      it { should have_login_shell '/bin/false' }
+      it { should belong_to_primary_group 'prometheus' }
+    end
+  end
 end
 
-describe file('/usr/local/bin/prometheus'), :if => os[:family] == 'ubuntu' do
-  it { should be_file }
-  it { should be_owned_by 'prometheus' }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe file('/usr/local/bin/prometheus') do
+      it { should be_file }
+      it { should be_owned_by 'prometheus' }
+    end
+  end
 end
 
-describe file('/usr/local/bin/promtool'), :if => os[:family] == 'ubuntu' do
-  it { should be_file }
-  it { should be_owned_by 'prometheus' }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe file('/usr/local/bin/promtool') do
+      it { should be_file }
+      it { should be_owned_by 'prometheus' }
+    end
+  end
 end
 
-describe file('/etc/prometheus'), :if => os[:family] == 'ubuntu' do
-  it { should be_directory }
-  it { should be_owned_by 'prometheus' }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe file('/etc/prometheus') do
+      it { should be_directory }
+      it { should be_owned_by 'prometheus' }
+    end
+  end
 end
 
-describe file('/etc/prometheus/console_libraries'), :if => os[:family] == 'ubuntu' do
-  it { should be_directory }
-  it { should be_owned_by 'prometheus' }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe file('/etc/prometheus/console_libraries') do
+      it { should be_directory }
+      it { should be_owned_by 'prometheus' }
+    end
+  end
 end
 
-describe file('/etc/prometheus/consoles'), :if => os[:family] == 'ubuntu' do
-  it { should be_directory }
-  it { should be_owned_by 'prometheus' }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe file('/etc/prometheus/consoles') do
+      it { should be_directory }
+      it { should be_owned_by 'prometheus' }
+    end
+  end
 end
 
-describe file('/var/lib/prometheus'), :if => os[:family] == 'ubuntu' do
-  it { should be_directory }
-  it { should be_owned_by 'prometheus' }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe file('/var/lib/prometheus') do
+      it { should be_directory }
+      it { should be_owned_by 'prometheus' }
+    end
+  end
 end
 
-describe file('/etc/prometheus/prometheus.yml'), :if => os[:family] == 'ubuntu' do
-  it { should be_file }
-  it { should be_owned_by 'prometheus' }
-  it { should contain 'scrape_interval:     5s' }
-  it { should contain 'evaluation_interval: 5s' }
-  it { should contain 'node' }
-  it { should contain 'localhost:9100' }
-  it { should contain 'prometheus' }
-  it { should contain 'localhost:9090' }
-  it { should contain 'alertmanager' }
-  it { should contain 'localhost:9093' }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe file('/etc/prometheus/prometheus.yml') do
+      it { should be_file }
+      it { should be_owned_by 'prometheus' }
+      it { should contain 'scrape_interval:     5s' }
+      it { should contain 'evaluation_interval: 5s' }
+      it { should contain 'node' }
+      it { should contain 'localhost:9100' }
+      it { should contain 'prometheus' }
+      it { should contain 'localhost:9090' }
+      it { should contain 'alertmanager' }
+      it { should contain 'localhost:9093' }
+    end
+  end
 end
