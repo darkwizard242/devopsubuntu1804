e6549c57c012e34bae9d49aa0af3f3028ad97d57
darkwizard242 <am900820@gmail.com>
2019-10-05 00:23:44 -0400

    node exporter serverspec updated

--- packer-build/serverspec-tests/spec/localhost/16-node_exporter_spec.rb
+++ packer-build/serverspec-tests/spec/localhost/16-node_exporter_spec.rb
@@ -1,21 +1,37 @@
 require 'spec_helper'
 
-describe service('node_exporter'), :if => os[:family] == 'ubuntu' do
-  it { should be_enabled }
-  it { should be_running }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe service('node_exporter') do
+      it { should be_enabled }
+      it { should be_running }
+    end
+  end
 end
 
-describe port(9100), :if => os[:family] == 'ubuntu' do
-  it { should be_listening }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe port(9100) do
+      it { should be_listening }
+    end
+  end
 end
 
-describe user('node_exporter') do
-  it { should exist }
-  it { should have_login_shell '/bin/false' }
-  it { should belong_to_primary_group 'node_exporter' }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe user('node_exporter') do
+      it { should exist }
+      it { should have_login_shell '/bin/false' }
+      it { should belong_to_primary_group 'node_exporter' }
+    end
+  end
 end
 
-describe file('/usr/local/bin/node_exporter'), :if => os[:family] == 'ubuntu' do
-  it { should be_file }
-  it { should be_owned_by 'node_exporter' }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe file('/usr/local/bin/node_exporter') do
+      it { should be_file }
+      it { should be_owned_by 'node_exporter' }
+    end
+  end
 end
