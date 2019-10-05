3b49e93de0ffb8d494d8904eb7e418c0a4a3d932
darkwizard242 <am900820@gmail.com>
2019-10-05 00:21:33 -0400

    azurecli serverspec updated

--- packer-build/serverspec-tests/spec/localhost/14-azurecli_spec.rb
+++ packer-build/serverspec-tests/spec/localhost/14-azurecli_spec.rb
@@ -1,19 +1,35 @@
 require 'spec_helper'
 
 
-describe file('/etc/apt/sources.list.d/azure-cli.list'), :if => os[:family] == 'ubuntu' do
-  it { should be_file }
-  it { should contain 'deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli bionic main' }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe file('/etc/apt/sources.list.d/azure-cli.list') do
+      it { should be_file }
+      it { should contain 'deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli bionic main' }
+    end
+  end
 end
 
-describe package('azure-cli'), :if => os[:family] == 'ubuntu' do
-  it { should be_installed }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe package('azure-cli') do
+      it { should be_installed }
+    end
+  end
 end
 
-describe file('/usr/bin/az'), :if => os[:family] == 'ubuntu' do
-  it { should be_file }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe file('/usr/bin/az') do
+      it { should be_file }
+    end
+  end
 end
 
-describe command('az --version'), :if => os[:family] == 'ubuntu' do
-  its(:exit_status) { should eq 0 }
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe command('az --version') do
+      its(:exit_status) { should eq 0 }
+    end
+  end
 end
