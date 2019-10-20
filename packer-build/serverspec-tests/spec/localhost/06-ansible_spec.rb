2c379160fb0a6af7e4c3b6ea5910eb8d12725c57
darkwizard242 <am900820@gmail.com>
2019-10-20 17:43:38 -0400

    increased spec test for ansible

--- packer-build/serverspec-tests/spec/localhost/06-ansible_spec.rb
+++ packer-build/serverspec-tests/spec/localhost/06-ansible_spec.rb
@@ -1,25 +1,28 @@
 require 'spec_helper'
 
+
 if os[:family] == 'ubuntu'
   if os[:release] == '18.04'
     describe ppa('ansible/ansible') do
       it { should exist }
       it { should be_enabled }
     end
-  end
-end
-
-if os[:family] == 'ubuntu'
-  if os[:release] == '18.04'
     describe package('ansible') do
       it { should be_installed }
     end
-  end
-end
-
-if os[:family] == 'ubuntu'
-  if os[:release] == '18.04'
+    describe file('/usr/bin/ansible') do
+      it { should exist }
+      it { should be_file }
+      it { should be_executable }
+    end
+    describe command('which ansible') do
+      its(:exit_status) { should eq 0 }
+      its(:stdout) { should match /\/usr\/bin\/ansible/ }
+    end
     describe command('ansible --version') do
+      its(:exit_status) { should eq 0 }
+    end
+    describe command('ansible localhost -m shell -a "hostname"') do
       its(:exit_status) { should eq 0 }
     end
   end
