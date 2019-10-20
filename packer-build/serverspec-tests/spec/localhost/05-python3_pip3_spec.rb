6494384330ae2c58957999d38e7b910190043afd
darkwizard242 <am900820@gmail.com>
2019-10-20 17:37:51 -0400

    increased spec test for python3 and python3-pip

--- packer-build/serverspec-tests/spec/localhost/05-python3_pip3_spec.rb
+++ packer-build/serverspec-tests/spec/localhost/05-python3_pip3_spec.rb
@@ -6,16 +6,22 @@ if os[:family] == 'ubuntu'
     describe package('python3') do
       it { should be_installed }
     end
-  end
-end
-
-if os[:family] == 'ubuntu'
-  if os[:release] == '18.04'
+    describe file('/usr/bin/python3') do
+      it { should exist }
+      it { should be_file }
+      it { should be_symlink }
+      it { should be_executable }
+    end
     describe command('python3 --version') do
       its(:exit_status) { should eq 0 }
     end
+    describe command('which python3') do
+      its(:exit_status) { should eq 0 }
+      its(:stdout) { should match /\/usr\/bin\/python3/ }
+    end
   end
 end
+
 
 ## Check for python3-pip package
 if os[:family] == 'ubuntu'
@@ -23,13 +29,17 @@ if os[:family] == 'ubuntu'
     describe package('python3-pip') do
       it { should be_installed }
     end
-  end
-end
-
-if os[:family] == 'ubuntu'
-  if os[:release] == '18.04'
+    describe file('/usr/local/bin/pip3') do
+      it { should exist }
+      it { should be_file }
+      it { should be_executable }
+    end
     describe command('pip3 --version') do
       its(:exit_status) { should eq 0 }
+    end
+    describe command('which pip3') do
+      its(:exit_status) { should eq 0 }
+      its(:stdout) { should match /\/usr\/local\/bin\/pip3/ }
     end
   end
 end
