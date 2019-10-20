c40e7092b2769ef0a00da199186e7528cbc7fc61
darkwizard242 <am900820@gmail.com>
2019-10-20 17:18:42 -0400

    increased spec test for git

--- packer-build/serverspec-tests/spec/localhost/04-git_spec.rb
+++ packer-build/serverspec-tests/spec/localhost/04-git_spec.rb
@@ -1,18 +1,22 @@
 require 'spec_helper'
 
+
 if os[:family] == 'ubuntu'
   if os[:release] == '18.04'
     describe package('git') do
       it { should be_installed }
     end
-  end
-end
-
-## Check if git command exits with a successful exit code.
-if os[:family] == 'ubuntu'
-  if os[:release] == '18.04'
+    describe file('/usr/bin/git') do
+      it { should exist }
+      it { should be_file }
+      it { should be_executable }
+    end
     describe command('git --version') do
       its(:exit_status) { should eq 0 }
+    end
+    describe command('which git') do
+      its(:exit_status) { should eq 0 }
+      its(:stdout) { should match /\/usr\/bin\/git/ }
     end
   end
 end
