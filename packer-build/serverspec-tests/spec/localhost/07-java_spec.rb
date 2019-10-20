cbbb8d32ba273aec8d7ce197d6c45e5cd68f0f4a
darkwizard242 <am900820@gmail.com>
2019-10-20 17:46:59 -0400

    increased spec test for java

--- packer-build/serverspec-tests/spec/localhost/07-java_spec.rb
+++ packer-build/serverspec-tests/spec/localhost/07-java_spec.rb
@@ -4,12 +4,18 @@ require 'spec_helper'
 if os[:family] == 'ubuntu'
   if os[:release] == '18.04'
     describe package('openjdk-8-jdk') do
+        it { should be_installed }
     end
-  end
-end
-
-if os[:family] == 'ubuntu'
-  if os[:release] == '18.04'
+    describe file('/usr/bin/java') do
+      it { should exist }
+      it { should be_file }
+      it { should be_symlink }
+      it { should be_executable }
+    end
+    describe command('which java') do
+      its(:exit_status) { should eq 0 }
+      its(:stdout) { should match /\/usr\/bin\/java/ }
+    end
     describe command('java -version') do
       its(:exit_status) { should eq 0 }
     end
