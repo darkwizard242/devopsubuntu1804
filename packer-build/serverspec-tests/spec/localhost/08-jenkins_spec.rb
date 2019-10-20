c7997cfa6063a43f712d99cc196fe83d1df54ff1
darkwizard242 <am900820@gmail.com>
2019-10-20 17:54:47 -0400

    remodel jenkins spec test

--- packer-build/serverspec-tests/spec/localhost/08-jenkins_spec.rb
+++ packer-build/serverspec-tests/spec/localhost/08-jenkins_spec.rb
@@ -1,36 +1,27 @@
 require 'spec_helper'
 
+
 ## Check if jenkins package is installed.
 if os[:family] == 'ubuntu'
   if os[:release] == '18.04'
+    describe file('/etc/apt/sources.list.d/jenkins.list') do
+      it { should exist }
+      it { should be_file }
+      it { should be_readable }
+      it { should be_writable.by_user('root') }
+      it { should be_mode 644 }
+      its(:content) { should match /deb\ http\:\/\/pkg\.jenkins\.io\/debian\-stable\ binary\// }
+    end
     describe package('jenkins') do
       it { should be_installed }
     end
-  end
-end
-
-## Check if jenkins service is enabled and running.
-if os[:family] == 'ubuntu'
-  if os[:release] == '18.04'
     describe service('jenkins') do
       it { should be_enabled }
       it { should be_running }
     end
-  end
-end
-
-## Check if listening on port 8080.
-if os[:family] == 'ubuntu'
-  if os[:release] == '18.04'
     describe port(8080) do
       it { should be_listening }
     end
-  end
-end
-
-## Check if the jenkins directory exists and it's ownership.
-if os[:family] == 'ubuntu'
-  if os[:release] == '18.04'
     describe file('/var/lib/jenkins') do
       it { should be_directory }
       it { should be_owned_by 'jenkins' }
