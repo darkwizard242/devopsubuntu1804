ec09e1f6fd1d357ef09a7b06f6ba02f5bdb7734b
darkwizard242 <am900820@gmail.com>
2019-11-17 16:23:03 -0500

    increased spec test for azure cli

--- packer-build/serverspec-tests/spec/localhost/14-azurecli_spec.rb
+++ packer-build/serverspec-tests/spec/localhost/14-azurecli_spec.rb
@@ -1,18 +1,53 @@
 require 'spec_helper'
 
-
 if os[:family] == 'ubuntu'
   if os[:release] == '18.04'
-    describe file('/etc/apt/sources.list.d/azure-cli.list') do
+    describe package('curl') do
+      it { should be_installed }
+    end
+    describe file('/usr/bin/curl') do
+      it { should exist }
       it { should be_file }
-      it { should contain 'deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli bionic main' }
+      it { should be_executable }
+    end
+    describe command('which curl') do
+      its(:exit_status) { should eq 0 }
+      its(:stdout) { should match /\/usr\/bin\/curl/ }
     end
   end
 end
 
+
 if os[:family] == 'ubuntu'
   if os[:release] == '18.04'
-    describe package('azure-cli') do
+    describe package('apt-transport-https') do
+      it { should be_installed }
+    end
+  end
+end
+
+
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe package('lsb-release') do
+      it { should be_installed }
+    end
+  end
+end
+
+
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe package('gnupg') do
+      it { should be_installed }
+    end
+  end
+end
+
+
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe package('software-properties-common') do
       it { should be_installed }
     end
   end
@@ -20,16 +55,28 @@ end
 
 if os[:family] == 'ubuntu'
   if os[:release] == '18.04'
-    describe file('/usr/bin/az') do
+    describe file('/etc/apt/sources.list.d/azure-cli.list') do
+      it { should exist }
       it { should be_file }
+      it { should be_readable }
+      it { should be_writable.by_user('root') }
+      it { should be_mode 644 }
+      its(:content) { should match /deb\ \[arch\=amd64\]\ https\:\/\/packages\.microsoft\.com\/repos\/azure\-cli\ bionic\ main/ }
     end
-  end
-end
-
-if os[:family] == 'ubuntu'
-  if os[:release] == '18.04'
+    describe package('azure-cli') do
+      it { should be_installed }
+    end
+    describe file('/usr/bin/az') do
+      it { should exist }
+      it { should be_file }
+      it { should be_executable }
+    end
     describe command('az --version') do
       its(:exit_status) { should eq 0 }
+    end
+    describe command('which az') do
+      its(:exit_status) { should eq 0 }
+      its(:stdout) { should match /\/usr\/bin\/az/ }
     end
   end
 end
