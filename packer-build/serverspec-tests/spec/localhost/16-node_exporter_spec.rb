99ad04bea23be2d3882902a3c579b1f14943c4f8
darkwizard242 <am900820@gmail.com>
2019-11-17 16:40:07 -0500

    increased spec test for node_exporter

--- packer-build/serverspec-tests/spec/localhost/16-node_exporter_spec.rb
+++ packer-build/serverspec-tests/spec/localhost/16-node_exporter_spec.rb
@@ -6,32 +6,30 @@ if os[:family] == 'ubuntu'
       it { should be_enabled }
       it { should be_running }
     end
-  end
-end
-
-if os[:family] == 'ubuntu'
-  if os[:release] == '18.04'
     describe port(9100) do
       it { should be_listening }
     end
-  end
-end
-
-if os[:family] == 'ubuntu'
-  if os[:release] == '18.04'
+    describe file('/usr/local/bin/node_exporter') do
+      it { should exist }
+      it { should be_file }
+      it { should be_owned_by 'node_exporter' }
+      it { should be_executable }
+    end
+    describe command('which node_exporter') do
+      its(:exit_status) { should eq 0 }
+      its(:stdout) { should match /\/usr\/local\/bin\/node_exporter/ }
+    end
     describe user('node_exporter') do
       it { should exist }
       it { should have_login_shell '/bin/false' }
       it { should belong_to_primary_group 'node_exporter' }
     end
-  end
-end
-
-if os[:family] == 'ubuntu'
-  if os[:release] == '18.04'
-    describe file('/usr/local/bin/node_exporter') do
+    describe file('/etc/systemd/system/node_exporter.service') do
+      it { should exist }
       it { should be_file }
-      it { should be_owned_by 'node_exporter' }
+      it { should be_owned_by 'root' }
+      it { should be_mode 644 }
+      its(:content) { should match /\/usr\/local\/bin\/node_exporter/ }
     end
   end
 end
