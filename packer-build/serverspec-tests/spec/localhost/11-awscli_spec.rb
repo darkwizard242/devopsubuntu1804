6fd39d91edf28846de68af53e42231c7da70396e
darkwizard242 <am900820@gmail.com>
2019-11-17 16:11:46 -0500

    executable spec test added for aws cli

--- packer-build/serverspec-tests/spec/localhost/11-awscli_spec.rb
+++ packer-build/serverspec-tests/spec/localhost/11-awscli_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 if os[:family] == 'ubuntu'
   if os[:release] == '18.04'
+    describe file('/usr/local/bin/aws') do
+      it { should exist }
+      it { should be_file }
+      it { should be_executable }
+    end
     describe command('aws --version') do
       its(:exit_status) { should eq 0 }
     end
