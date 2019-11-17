32d7bc605e7323bc3b111f30bfcd3f8c15b8361c
darkwizard242 <am900820@gmail.com>
2019-11-17 16:30:22 -0500

    increased spec test for google cloud sdk cli

--- packer-build/serverspec-tests/spec/localhost/15-gcloudsdk_spec.rb
+++ packer-build/serverspec-tests/spec/localhost/15-gcloudsdk_spec.rb
@@ -4,32 +4,27 @@ require 'spec_helper'
 if os[:family] == 'ubuntu'
   if os[:release] == '18.04'
     describe file('/etc/apt/sources.list.d/google-cloud-sdk.list') do
+      it { should exist }
       it { should be_file }
-      it { should contain 'deb http://packages.cloud.google.com/apt cloud-sdk-bionic main' }
+      it { should be_readable }
+      it { should be_writable.by_user('root') }
+      it { should be_mode 644 }
+      its(:content) { should match /deb\ http\:\/\/packages\.cloud\.google\.com\/apt\ cloud\-sdk\-bionic\ main/ }
     end
-  end
-end
-
-if os[:family] == 'ubuntu'
-  if os[:release] == '18.04'
     describe package('google-cloud-sdk') do
       it { should be_installed }
     end
-  end
-end
-
-if os[:family] == 'ubuntu'
-  if os[:release] == '18.04'
     describe file('/usr/bin/gcloud') do
-      it { should be_file }
+      it { should exist }
+      it { should be_symlink }
+      it { should be_executable }
     end
-  end
-end
-
-if os[:family] == 'ubuntu'
-  if os[:release] == '18.04'
     describe command('gcloud --version') do
       its(:exit_status) { should eq 0 }
+    end
+    describe command('which gcloud') do
+      its(:exit_status) { should eq 0 }
+      its(:stdout) { should match /\/usr\/bin\/gcloud/ }
     end
   end
 end
