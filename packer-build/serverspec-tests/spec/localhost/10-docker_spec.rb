9212bd15b733f2c15006e9028ad93f11d3e2822a
darkwizard242 <am900820@gmail.com>
2019-11-17 16:02:34 -0500

    additional testing params for docker

--- packer-build/serverspec-tests/spec/localhost/10-docker_spec.rb
+++ packer-build/serverspec-tests/spec/localhost/10-docker_spec.rb
@@ -78,6 +78,14 @@ if os[:family] == 'ubuntu'
       it { should be_enabled }
       it { should be_running }
     end
+    describe file('/var/lib/docker') do
+      it { should exist }
+      it { should be_directory }
+      it { should be_writable.by_user('root') }
+      it { should be_readable.by_user('root') }
+      it { should be_executable }
+      it { should be_mode 711 }
+    end
     describe file('/var/run/docker.sock') do
       it { should be_socket }
       it { should be_owned_by 'root' }
