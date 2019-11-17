9720c3134b8b0025ab31617e1cd6e263012483be
darkwizard242 <am900820@gmail.com>
2019-11-17 15:34:24 -0500

    increased spec test for docker

--- packer-build/serverspec-tests/spec/localhost/10-docker_spec.rb
+++ packer-build/serverspec-tests/spec/localhost/10-docker_spec.rb
@@ -3,43 +3,98 @@ require 'spec_helper'
 
 if os[:family] == 'ubuntu'
   if os[:release] == '18.04'
-    describe file('/etc/apt/sources.list.d/docker.list') do
-      it { should be_file }
-      it { should contain 'deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable' }
-    end
-  end
-end
-
-if os[:family] == 'ubuntu'
-  if os[:release] == '18.04'
-    describe package('docker-ce') do
+    describe package('apt-transport-https') do
       it { should be_installed }
     end
   end
 end
 
+
 if os[:family] == 'ubuntu'
   if os[:release] == '18.04'
+    describe package('ca-certificates') do
+      it { should be_installed }
+    end
+    describe file('/usr/sbin/update-ca-certificates') do
+      it { should exist }
+      it { should be_file }
+      it { should be_executable }
+    end
+    describe command('which update-ca-certificates') do
+      its(:exit_status) { should eq 0 }
+      its(:stdout) { should match /\/usr\/sbin\/update-ca-certificates/ }
+    end
+  end
+end
+
+
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe package('curl') do
+      it { should be_installed }
+    end
+    describe file('/usr/bin/curl') do
+      it { should exist }
+      it { should be_file }
+      it { should be_executable }
+    end
+    describe command('which curl') do
+      its(:exit_status) { should eq 0 }
+      its(:stdout) { should match /\/usr\/bin\/curl/ }
+    end
+  end
+end
+
+
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe package('software-properties-common') do
+      it { should be_installed }
+    end
+  end
+end
+
+
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe file('/etc/apt/sources.list.d/docker.list') do
+      it { should exist }
+      it { should be_file }
+      it { should be_readable }
+      it { should be_writable.by_user('root') }
+      it { should be_mode 644 }
+      its(:content) { should match /deb\ \[arch\=amd64\]\ https\:\/\/download\.docker\.com\/linux\/ubuntu\ bionic\ stable/ }
+    end
+    describe package('docker-ce') do
+      it { should be_installed }
+    end
+    describe package('docker-ce-cli') do
+      it { should be_installed }
+    end
+    describe package('containerd.io') do
+      it { should be_installed }
+    end
     describe service('docker') do
       it { should be_enabled }
       it { should be_running }
     end
-  end
-end
-
-if os[:family] == 'ubuntu'
-  if os[:release] == '18.04'
     describe file('/var/run/docker.sock') do
       it { should be_socket }
       it { should be_owned_by 'root' }
     end
-  end
-end
-
-if os[:family] == 'ubuntu'
-  if os[:release] == '18.04'
     describe command('docker --version') do
       its(:exit_status) { should eq 0 }
+    end
+    describe command('which docker') do
+      its(:exit_status) { should eq 0 }
+      its(:stdout) { should match /\/usr\/bin\/docker/ }
+    end
+    describe command('containerd --version') do
+      its(:exit_status) { should eq 0 }
+    end
+    describe command('which containerd') do
+      its(:exit_status) { should eq 0 }
+      its(:stdout) { should match /\/usr\/bin\/containerd/ }
     end
   end
 end
