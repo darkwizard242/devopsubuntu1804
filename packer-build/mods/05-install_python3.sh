b083c9288931126ca7b0445f8cfed02932e5c3c7
darkwizard242 <am900820@gmail.com>
2019-12-07 00:16:36 -0500

    remodel to arg based python3 script

---
+++ packer-build/mods/05-install_python3.sh
@@ -0,0 +1,59 @@
+#!/bin/bash -e
+
+# Shellcheck fixes for: SC2086, SC2181, SC2006
+
+package1="python3"
+
+check_os () {
+  if [ "$(grep -Ei 'VERSION_ID="16.04"' /etc/os-release)" ];
+  then
+    echo -e "\nSystem OS is Ubuntu. Version is 16.04.\n\n###\tProceeding with SCRIPT Execution\t###\n"
+  elif [ "$(grep -Ei 'VERSION_ID="18.04"' /etc/os-release)" ];
+  then
+    echo -e "\nSystem OS is Ubuntu. Version is 18.04.\n\n###\tProceeding with SCRIPT Execution\t###\n"
+  else
+    echo -e "\nThis is neither Ubuntu 16.04 or Ubuntu 18.04.\n\n###\tScript execution HALTING!\t###\n"
+    exit 2
+  fi
+}
+
+check_if_python3_installed () {
+  if ${package1} --version &> /dev/null;
+    then
+      echo -e "\nYES: ${package1} is IN an installed state within the system.\n"
+      exit 0
+    else
+      echo -e "\nNO: ${package1} is NOT IN an installed state.\n"
+  fi
+}
+
+python3_installer () {
+  DEBIAN_FRONTEND=non-interactive apt-get install ${package1} -y
+}
+
+python3_uninstaller () {
+  DEBIAN_FRONTEND=non-interactive apt-get purge ${package1} -y
+}
+
+case "$1" in
+  check)
+    check_os
+    check_if_python3_installed
+    ;;
+  install)
+    check_os
+    check_if_python3_installed
+    echo -e "\nInstallation beginning for:\t${package1}\n"
+    python3_installer
+    ;;
+  uninstall)
+    check_os
+    echo -e "\nPurging beginning for:\t${package1}\n"
+    python3_uninstaller
+    ;;
+  *)
+    echo -e $"\nUsage:\t $0 check\t\t : Checks if ${package1} is installed on the system."
+    echo -e $"Usage:\t $0 install\t\t : For installing ${package1} from the system."
+    echo -e $"Usage:\t $0 uninstall\t\t : For uninstalling/purging ${package1} from the system.\n"
+    exit 1
+esac
