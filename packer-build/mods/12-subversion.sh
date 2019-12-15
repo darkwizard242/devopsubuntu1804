52111bf735314d2069c54f052882bf8c78f6e394
darkwizard242 <am900820@gmail.com>
2019-12-14 23:02:25 -0500

    remodel to arg based subversion script

---
+++ packer-build/mods/12-subversion.sh
@@ -0,0 +1,60 @@
+#!/bin/bash -e
+
+# Shellcheck fixes for: SC2181
+
+package="subversion"
+
+check_os () {
+  if [ "$(grep -Ei 'VERSION_ID="16.04"' /etc/os-release)" ];
+  then
+    echo -e "\nSystem OS is Ubuntu. Version is 16.04.\n\n###\tProceeding with SCRIPT Execution\t###\n"
+  elif [ "$(grep -Ei 'VERSION_ID="18.04"' /etc/os-release)" ];
+  then
+    echo -e "\nSystem OS is Ubuntu. Version is 18.04.\n\n###\tProceeding with SCRIPT Execution\t###\n"
+  else
+    echo -e "\nThis is neither Ubuntu 16.04 or Ubuntu 18.04.\n\n###\tScript execution HALTING!\t###\n"
+    exit 2
+  fi
+}
+
+check_if_subversion_installed () {
+  if svn --version &> /dev/null;
+    then
+      echo -e "\nYES: ${package} is IN an installed state within the system.\n"
+      exit 0
+    else
+      echo -e "\nNO: ${package} is NOT IN an installed state.\n"
+  fi
+}
+
+subversion_installer () {
+  DEBIAN_FRONTEND=non-interactive apt-get install ${package} -y
+}
+
+subversion_uninstaller () {
+  DEBIAN_FRONTEND=non-interactive apt-get purge ${package} -y
+}
+
+
+case "$1" in
+  check)
+    check_os
+    check_if_subversion_installed
+    ;;
+  install)
+    check_os
+    check_if_subversion_installed
+    echo -e "\nInstallation beginning for:\t${package}\n"
+    subversion_installer
+    ;;
+  uninstall)
+    check_os
+    echo -e "\nPurging beginning for:\t${package}\n"
+    subversion_uninstaller
+    ;;
+  *)
+    echo -e $"\nUsage:\t $0 check\t\t : Checks if ${package} is installed on the system."
+    echo -e $"Usage:\t $0 install\t : For installing ${package} from the system."
+    echo -e $"Usage:\t $0 uninstall\t : For uninstalling/purging ${package} from the system.\n"
+    exit 1
+esac
