04182d7fce08484dc3bb5f12b5e47d9836dec78f
darkwizard242 <am900820@gmail.com>
2019-12-25 19:55:28 -0500

    motd devops file path to move from - updated

--- packer-build/mods/01-init.sh
+++ packer-build/mods/01-init.sh
@@ -49,6 +49,6 @@ done
 # Disable all current motd's and only use custom motd.
 ls -altr /home/vagrant/
 chmod -v -x /etc/update-motd.d/*
-mv -v /home/vagrant/00-devops /etc/update-motd.d/ && ls -altr /etc/update-motd.d/
+mv -v /home/vagrant/mods/00-devops /etc/update-motd.d/ && ls -altr /etc/update-motd.d/
 chown -Rv root:root /etc/update-motd.d/00-devops
 chmod -v a+x /etc/update-motd.d/00-devops && ls -altr /etc/update-motd.d/
