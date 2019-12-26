0f76d15885840d736bb6f8db915db31f849a6f06
darkwizard242 <am900820@gmail.com>
2019-12-26 16:13:12 -0500

    use tee to add jenkins repo file

--- packer-build/mods/08-jenkins.sh
+++ packer-build/mods/08-jenkins.sh
@@ -27,7 +27,7 @@ check_if_jenkins_installed () {
 
 add_jenkins_repo () {
   wget -q -O - https://pkg.${package}.io/debian/${package}.io.key | sudo apt-key add -
-  echo "deb http://pkg.${package}.io/debian-stable binary/" > /etc/apt/sources.list.d/${package}.list
+  echo "deb http://pkg.${package}.io/debian-stable binary/" | sudo tee -a /etc/apt/sources.list.d/${package}.list
   DEBIAN_FRONTEND=non-interactive apt-get update
 }
 
