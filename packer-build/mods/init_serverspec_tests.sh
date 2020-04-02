d86fcfdeabf8cbab74d91297b4935188ff230ff2
darkwizard242 <am900820@gmail.com>
2020-04-02 18:04:18 -0400

    enable serverspec testing

--- packer-build/mods/init_serverspec_tests.sh
+++ packer-build/mods/init_serverspec_tests.sh
@@ -7,17 +7,17 @@ serverspec_dest_dir="/opt/serverspec-tests"
 rsync -avzh /home/vagrant/serverspec-tests /opt/
 rm -rfv /home/vagrant/serverspec-tests
 chown -Rv root:root /opt/serverspec-tests
-cd $serverspec_dest_dir
+cd ${serverspec_dest_dir}
 
 if [ "$PWD" = "$serverspec_dest_dir" ];
   then
     echo -e "\n\tCurrent Directory is: $PWD \n"
     echo -e "\n\tExecuting ServerSpec tests: \n"
-    #rake spec:localhost
+    rake spec:localhost
   else
     echo -e "\nCurrent Directory is $PWD and not the correct directory to run server-spec tests in!\n"
-    echo -e "\nChanging to desired directory: serverspec_dest_dir \n" && cd $serverspec_dest_dir
+    echo -e "\nChanging to desired directory: serverspec_dest_dir \n" && cd ${serverspec_dest_dir}
     echo -e "\n\tCurrent Directory is: $PWD \n"
     echo -e "\n\tExecuting ServerSpec tests: \n"
-    #rake spec:localhost
+    rake spec:localhost
 fi
