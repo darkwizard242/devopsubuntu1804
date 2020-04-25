6502d5522f05c0209f02c8fbde2e1217f07fb004
darkwizard242 <am900820@gmail.com>
2020-04-24 23:35:45 -0400

    remodel google-cloud-sdk script for ubuntu 20.04

--- packer-build/mods/15-gcloudsdk.sh
+++ packer-build/mods/15-gcloudsdk.sh
@@ -7,20 +7,23 @@ sdk_rel="cloud-sdk-$(lsb_release -cs)"
 package="google-cloud-sdk"
 
 
-check_os () {
+function check_os () {
   if [ "$(grep -Ei 'VERSION_ID="16.04"' /etc/os-release)" ];
   then
     echo -e "\nSystem OS is Ubuntu. Version is 16.04.\n\n###\tProceeding with SCRIPT Execution\t###\n"
   elif [ "$(grep -Ei 'VERSION_ID="18.04"' /etc/os-release)" ];
   then
     echo -e "\nSystem OS is Ubuntu. Version is 18.04.\n\n###\tProceeding with SCRIPT Execution\t###\n"
+  elif [ "$(grep -Ei 'VERSION_ID="20.04"' /etc/os-release)" ];
+  then
+    echo -e "\nSystem OS is Ubuntu. Version is 20.04.\n\n###\tProceeding with SCRIPT Execution\t###\n"
   else
-    echo -e "\nThis is neither Ubuntu 16.04 or Ubuntu 18.04.\n\n###\tScript execution HALTING!\t###\n"
+    echo -e "\nThis is neither Ubuntu 16.04, Ubuntu 18.04 or Ubuntu 20.04.\n\n###\tScript execution HALTING!\t###\n"
     exit 2
   fi
 }
 
-setup_dependencies () {
+function setup_dependencies () {
   for dependency in ${dependencies};
   do
     if dpkg -s "${dependency}" &> /dev/null;
@@ -33,7 +36,7 @@ setup_dependencies () {
   done
 }
 
-check_if_google-cloud-sdk_installed () {
+function check_if_google-cloud-sdk_installed () {
   if gcloud --version &> /dev/null;
     then
       echo -e "\nYES: ${package} is IN an installed state within the system.\n"
@@ -43,26 +46,26 @@ check_if_google-cloud-sdk_installed () {
   fi
 }
 
-add_google-cloud-sdk_repo () {
+function add_google-cloud-sdk_repo () {
   echo -e "\nAdding ${package} key and repo to apt list!"
   curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
   echo -e "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/${package}.list
   DEBIAN_FRONTEND=non-interactive apt-get update
 }
 
-google-cloud-sdk_installer () {
+function google-cloud-sdk_installer () {
   DEBIAN_FRONTEND=non-interactive apt-get install ${package} -y
 }
 
-google-cloud-sdk_uninstaller () {
+function google-cloud-sdk_uninstaller () {
   DEBIAN_FRONTEND=non-interactive apt-get purge ${package} -y
 }
 
-remove_google-cloud-sdk_repo () {
+function remove_google-cloud-sdk_repo () {
   rm -v /etc/apt/sources.list.d/${package}.list
 }
 
-google-cloud-sdk_verify () {
+function google-cloud-sdk_verify () {
   if gcloud --help &> /dev/null;
     then
       echo -e "\nExit status for ${package} command returned back with a successful exit code.\n"
