fe5b7f40ec434cd83073f79f617deec6540f2e5f
darkwizard242 <am900820@gmail.com>
2020-04-25 16:35:40 -0400

    serverspec suite enhanced for aws-vault

--- packer-build/serverspec-tests/spec/localhost/20-aws-vault_spec.rb
+++ packer-build/serverspec-tests/spec/localhost/20-aws-vault_spec.rb
@@ -14,13 +14,31 @@ if os[:family] == 'ubuntu'
       it { should be_writable.by_user('root') }
       it { should be_mode 755 }
     end
+  end
+end
+
+
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
     describe command('aws-vault --version') do
       its(:exit_status) { should eq 0 }
     end
+  end
+end
+
+
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
     describe command('which aws-vault') do
       its(:exit_status) { should eq 0 }
       its(:stdout) { should match /\/usr\/local\/bin\/aws-vault/ }
     end
+  end
+end
+
+
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
     describe file('/home/vagrant/.bashrc') do
       it { should exist }
       it { should be_file }
