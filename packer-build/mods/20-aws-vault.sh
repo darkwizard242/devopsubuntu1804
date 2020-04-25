ed740e1026b4ae2505d161faf9639d93c2239028
darkwizard242 <am900820@gmail.com>
2020-04-24 23:57:10 -0400

    remodel aws-vault script for ubuntu 20.04

--- packer-build/mods/20-aws-vault.sh
+++ packer-build/mods/20-aws-vault.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 # Shellcheck fixes for: SC2181, SC2006, SC2140
 
@@ -8,20 +8,23 @@ osarch="linux-amd64"
 package="aws-vault"
 extract_path="/usr/local/bin"
 
-check_os () {
+function check_os () {
   if [ "$(grep -Ei 'VERSION_ID="16.04"' /etc/os-release)" ];
   then
     echo -e "\nSystem OS is Ubuntu. Version is 16.04.\n\n###\tProceeding with SCRIPT Execution\t###\n"
   elif [ "$(grep -Ei 'VERSION_ID="18.04"' /etc/os-release)" ];
   then
     echo -e "\nSystem OS is Ubuntu. Version is 18.04.\n\n###\tProceeding with SCRIPT Execution\t###\n"
+  elif [ "$(grep -Ei 'VERSION_ID="20.04"' /etc/os-release)" ];
+  then
+    echo -e "\nSystem OS is Ubuntu. Version is 20.04.\n\n###\tProceeding with SCRIPT Execution\t###\n"
   else
-    echo -e "\nThis is neither Ubuntu 16.04 or Ubuntu 18.04.\n\n###\tScript execution HALTING!\t###\n"
+    echo -e "\nThis is neither Ubuntu 16.04, Ubuntu 18.04 or Ubuntu 20.04.\n\n###\tScript execution HALTING!\t###\n"
     exit 2
   fi
 }
 
-setup_dependencies () {
+function setup_dependencies () {
   for dependency in ${dependencies};
   do
     if dpkg -s "${dependency}" &> /dev/null;
@@ -34,7 +37,7 @@ setup_dependencies () {
   done
 }
 
-check_if_aws-vault_installed () {
+function check_if_aws-vault_installed () {
   if command -v ${package} &> /dev/null;
     then
       echo -e "\nYES: ${package} is IN an installed state within the system.\n"
@@ -44,20 +47,20 @@ check_if_aws-vault_installed () {
   fi
 }
 
-add_aws-vault_profile_export () {
+function add_aws-vault_profile_export () {
   echo -e "\n\nexport AWS_VAULT_BACKEND=\"file\"" >> "$HOME"/.bashrc
 }
 
-remove_aws-vault_profile_export () {
+function remove_aws-vault_profile_export () {
   sed -i "s/export\ AWS\_VAULT\_BACKEND\=\"file\"//g" "$HOME"/.bashrc
 }
 
-aws-vault_installer () {
+function aws-vault_installer () {
   curl -L -o ${extract_path}/${package} https://github.com/99designs/${package}/releases/download/v${version}/${package}-${osarch} &> /dev/null
   chmod -v 0755 ${extract_path}/${package}
 }
 
-aws-vault_uninstaller () {
+function aws-vault_uninstaller () {
   rm -v ${extract_path}/${package}
 }
 
