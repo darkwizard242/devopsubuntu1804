58c1ab1bb4ab7fc5ff4e2a218dc76b8e3cc35d9c
darkwizard242 <am900820@gmail.com>
2020-04-25 00:31:10 -0400

    remodel awless script for ubuntu 20.04

--- packer-build/mods/30-awless.sh
+++ packer-build/mods/30-awless.sh
@@ -8,20 +8,23 @@ osarch="linux-amd64"
 package="awless"
 extract_path="/usr/local/bin"
 
-check_os () {
+function check_os () {
   if [ "$(grep -Ei 'VERSION_ID="16.04"' /etc/os-release)" ];
   then
     echo -e "\nSystem OS is Ubuntu. Version is 16.04.\n\n###\tProceeding with SCRIPT Execution\t###\n"
   elif [ "$(grep -Ei 'VERSION_ID="18.04"' /etc/os-release)" ];
   then
     echo -e "\nSystem OS is Ubuntu. Version is 18.04.\n\n###\tProceeding with SCRIPT Execution\t###\n"
+  elif [ "$(grep -Ei 'VERSION_ID="20.04"' /etc/os-release)" ];
+  then
+    echo -e "\nSystem OS is Ubuntu. Version is 20.04.\n\n###\tProceeding with SCRIPT Execution\t###\n"
   else
-    echo -e "\nThis is neither Ubuntu 16.04 or Ubuntu 18.04.\n\n###\tScript execution HALTING!\t###\n"
+    echo -e "\nThis is neither Ubuntu 16.04, Ubuntu 18.04 or Ubuntu 20.04.\n\n###\tScript execution HALTING!\t###\n"
     exit 2
   fi
 }
 
-setup_dependencies () {
+function setup_dependencies () {
   for dependency in ${dependencies};
   do
     if dpkg -s "${dependency}" &> /dev/null;
@@ -34,7 +37,7 @@ setup_dependencies () {
   done
 }
 
-check_if_awless_installed () {
+function check_if_awless_installed () {
   if command -v ${package} &> /dev/null;
     then
       echo -e "\nYES: ${package} is IN an installed state within the system.\n"
@@ -44,14 +47,14 @@ check_if_awless_installed () {
   fi
 }
 
-awless_installer () {
+function awless_installer () {
   curl -L -o /tmp/${package}.tar.gz https://github.com/wallix/${package}/releases/download/v${version}/${package}-${osarch}.tar.gz &> /dev/null
   tar -xzf /tmp/${package}.tar.gz -C ${extract_path}/ && rm /tmp/${package}.tar.gz
   chmod -v 0755 ${extract_path}/${package}
   chown root:root ${extract_path}/${package}
 }
 
-awless_uninstaller () {
+function awless_uninstaller () {
   rm -v ${extract_path}/${package}
 }
 
