761087a6de0c1a4a381f643ef0710e9c89df4785
darkwizard242 <am900820@gmail.com>
2020-04-25 16:55:36 -0400

    serverspec enhance and modularise git testsuite

--- packer-build/serverspec-tests/spec/localhost/04-git_spec.rb
+++ packer-build/serverspec-tests/spec/localhost/04-git_spec.rb
@@ -6,14 +6,32 @@ if os[:family] == 'ubuntu'
     describe package('git') do
       it { should be_installed }
     end
+  end
+end
+
+
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
     describe file('/usr/bin/git') do
       it { should exist }
       it { should be_file }
       it { should be_executable }
     end
+  end
+end
+
+
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
     describe command('git --version') do
       its(:exit_status) { should eq 0 }
     end
+  end
+end
+
+
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
     describe command('which git') do
       its(:exit_status) { should eq 0 }
       its(:stdout) { should match /\/usr\/bin\/git/ }
