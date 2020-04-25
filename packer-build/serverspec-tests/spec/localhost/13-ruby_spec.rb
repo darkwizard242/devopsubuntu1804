912a6558a66fc01b7d09413670a348cbc85768fb
darkwizard242 <am900820@gmail.com>
2020-04-25 16:32:53 -0400

    serverspec - isolate ruby test cases and enhance

--- packer-build/serverspec-tests/spec/localhost/13-ruby_spec.rb
+++ packer-build/serverspec-tests/spec/localhost/13-ruby_spec.rb
@@ -5,17 +5,16 @@ if os[:family] == 'ubuntu'
     describe package('ruby') do
       it { should be_installed }
     end
+  end
+end
+
+
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
     describe file('/usr/bin/ruby') do
       it { should exist }
       it { should be_symlink }
       it { should be_executable }
-    end
-    describe command('ruby --version') do
-      its(:exit_status) { should eq 0 }
-    end
-    describe command('which ruby') do
-      its(:exit_status) { should eq 0 }
-      its(:stdout) { should match /\/usr\/bin\/ruby/ }
     end
   end
 end
@@ -23,8 +22,18 @@ end
 
 if os[:family] == 'ubuntu'
   if os[:release] == '18.04'
-    describe package('serverspec') do
-      it { should be_installed.by('gem') }
+    describe command('ruby --version') do
+      its(:exit_status) { should eq 0 }
+    end
+  end
+end
+
+
+if os[:family] == 'ubuntu'
+  if os[:release] == '18.04'
+    describe command('which ruby') do
+      its(:exit_status) { should eq 0 }
+      its(:stdout) { should match /\/usr\/bin\/ruby/ }
     end
   end
 end
