c83f74556831e61604a0f87dbf3ddf79609a4652
darkwizard242 <am900820@gmail.com>
2020-04-24 22:51:54 -0400

    remodel pip3 script for ubuntu 20.04

--- packer-build/mods/05.1-pip3.sh
+++ packer-build/mods/05.1-pip3.sh
@@ -5,20 +5,23 @@
 package1="python3"
 package2="python3-pip"
 
-check_os () {
+function check_os () {
   if [ "$(grep -Ei 'VERSION_ID="16.04"' /etc/os-release)" ];
   then
     echo -e "\nSystem OS is Ubuntu. Version is 16.04.\n\n###\tProceeding with SCRIPT Execution\t###\n"
   elif [ "$(grep -Ei 'VERSION_ID="18.04"' /etc/os-release)" ];
   then
     echo -e "\nSystem OS is Ubuntu. Version is 18.04.\n\n###\tProceeding with SCRIPT Execution\t###\n"
+  elif [ "$(grep -Ei 'VERSION_ID="20.04"' /etc/os-release)" ];
+  then
+    echo -e "\nSystem OS is Ubuntu. Version is 20.04.\n\n###\tProceeding with SCRIPT Execution\t###\n"
   else
-    echo -e "\nThis is neither Ubuntu 16.04 or Ubuntu 18.04.\n\n###\tScript execution HALTING!\t###\n"
+    echo -e "\nThis is neither Ubuntu 16.04, Ubuntu 18.04 or Ubuntu 20.04.\n\n###\tScript execution HALTING!\t###\n"
     exit 2
   fi
 }
 
-check_if_python3_installed () {
+function check_if_python3_installed () {
   if ${package1} --version &> /dev/null;
     then
       echo -e "\nYES: ${package1} is IN an installed state within the system.\n"
@@ -28,7 +31,7 @@ check_if_python3_installed () {
   fi
 }
 
-check_if_pip3_installed () {
+function check_if_pip3_installed () {
   if ${package1} -m pip --version &> /dev/null;
     then
       echo -e "\nYES: ${package2} is IN an installed state within the system.\n"
@@ -38,11 +41,11 @@ check_if_pip3_installed () {
   fi
 }
 
-pip3_installer () {
+function pip3_installer () {
   DEBIAN_FRONTEND=non-interactive apt-get install ${package2} -y
 }
 
-pip3_upgrade () {
+function pip3_upgrade () {
   check_if_python3_installed
   if dpkg -s ${package2} &> /dev/null;
     then
@@ -54,7 +57,7 @@ pip3_upgrade () {
   fi
 }
 
-pip3_uninstaller () {
+function pip3_uninstaller () {
   DEBIAN_FRONTEND=non-interactive apt-get purge ${package2} -y
 }
 
