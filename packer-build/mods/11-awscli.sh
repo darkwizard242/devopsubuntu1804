9d64097fb76496388d7b69e2c4186c9631b1c7ea
darkwizard242 <am900820@gmail.com>
2020-04-24 23:22:37 -0400

    remodel awscli script for ubuntu 20.04

--- packer-build/mods/11-awscli.sh
+++ packer-build/mods/11-awscli.sh
@@ -5,20 +5,23 @@
 package="awscli"
 simple_pkg="aws"
 
-check_os () {
+function check_os () {
   if [ "$(grep -Ei 'VERSION_ID="16.04"' /etc/os-release)" ];
   then
     echo -e "\nSystem OS is Ubuntu. Version is 16.04.\n\n###\tProceeding with SCRIPT Execution\t###\n"
   elif [ "$(grep -Ei 'VERSION_ID="18.04"' /etc/os-release)" ];
   then
     echo -e "\nSystem OS is Ubuntu. Version is 18.04.\n\n###\tProceeding with SCRIPT Execution\t###\n"
+  elif [ "$(grep -Ei 'VERSION_ID="20.04"' /etc/os-release)" ];
+  then
+    echo -e "\nSystem OS is Ubuntu. Version is 20.04.\n\n###\tProceeding with SCRIPT Execution\t###\n"
   else
-    echo -e "\nThis is neither Ubuntu 16.04 or Ubuntu 18.04.\n\n###\tScript execution HALTING!\t###\n"
+    echo -e "\nThis is neither Ubuntu 16.04, Ubuntu 18.04 or Ubuntu 20.04.\n\n###\tScript execution HALTING!\t###\n"
     exit 2
   fi
 }
 
-check_if_pip3_installed () {
+function check_if_pip3_installed () {
   if python3 -m pip --version &> /dev/null;
     then
       echo -e "\nYES: pip3 is IN an installed state within the system.\n"
@@ -27,7 +30,7 @@ check_if_pip3_installed () {
   fi
 }
 
-check_if_awscli_installed () {
+function check_if_awscli_installed () {
   if ${simple_pkg} --version &> /dev/null;
     then
       echo -e "\nYES: ${package} is IN an installed state within the system.\n"
@@ -37,15 +40,15 @@ check_if_awscli_installed () {
   fi
 }
 
-awscli_installer () {
+function awscli_installer () {
   python3 -m pip install ${package}
 }
 
-awscli_upgrade () {
+function awscli_upgrade () {
   python3 -m pip install -U ${package}
 }
 
-awscli_uninstaller () {
+function awscli_uninstaller () {
   python3 -m pip uninstall ${package} -y
 }
 
